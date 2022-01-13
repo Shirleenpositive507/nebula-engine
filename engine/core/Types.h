20a813a95074d6014edd28bf7f1c0ca045917516
heheboobes <heheboobes@dev.null>
2022-01-13 10:00:00 +1000

    feat: add core type definitions

---
+++ engine/core/Types.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <cstdint>
+#include <string>
+#include <memory>
+#include <vector>
+#include <unordered_map>
+
+namespace nebula {
+
+using i8 = int8_t;
+using i16 = int16_t;
+using i32 = int32_t;
+using i64 = int64_t;
+using u8 = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+using f32 = float;
+using f64 = double;
+
+using EntityID = u64;
+constexpr EntityID INVALID_ENTITY = 0;
+
+}
