dfe9026683e38aa1a599847f9bec2ba664743426
heheboobes <heheboobes@dev.null>
2022-01-14 10:00:00 +1000

    feat: add engine constants

---
+++ engine/core/Constants.h
@@ -0,0 +1,11 @@
+#pragma once
+namespace nebula {
+    constexpr int DEFAULT_WINDOW_WIDTH = 1280;
+    constexpr int DEFAULT_WINDOW_HEIGHT = 720;
+    constexpr const char* DEFAULT_WINDOW_TITLE = "Nebula Engine";
+    constexpr int FPS_LIMIT = 60;
+    constexpr float FIXED_TIMESTEP = 1.0f / 60.0f;
+    constexpr int MAX_ENTITIES = 10000;
+    constexpr int MAX_COMPONENTS = 64;
+    constexpr int MAX_SOUND_CHANNELS = 32;
+}
