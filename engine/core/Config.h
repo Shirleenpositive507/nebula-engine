d0a2b87b03a5ff55d42f057cb2fc439dba55ef03
heheboobes <heheboobes@dev.null>
2022-01-20 10:00:00 +1000

    feat: add configuration manager

---
+++ engine/core/Config.h
@@ -0,0 +1,72 @@
+#pragma once
+
+#include <string>
+#include <unordered_map>
+#include <sstream>
+#include <fstream>
+#include <algorithm>
+#include <cctype>
+#include <vector>
+
+namespace nebula {
+
+    class Config {
+    public:
+        static Config& instance();
+
+        bool load(const std::string& path);
+        bool save(const std::string& path);
+
+        template<typename T>
+        T get(const std::string& key, const T& defaultValue = T()) const {
+            auto it = m_values.find(key);
+            if (it == m_values.end()) return defaultValue;
+
+            T value;
+            std::istringstream iss(it->second);
+            if constexpr (std::is_same_v<T, bool>) {
+                std::string str = it->second;
+                std::transform(str.begin(), str.end(), str.begin(), ::tolower);
+                return str == "true" || str == "1" || str == "yes";
+            } else if constexpr (std::is_same_v<T, std::string>) {
+                return it->second;
+            } else {
+                iss >> value;
+            }
+            return value;
+        }
+
+        template<typename T>
+        void set(const std::string& key, const T& value) {
+            if constexpr (std::is_same_v<T, bool>) {
+                m_values[key] = value ? "true" : "false";
+            } else {
+                std::ostringstream oss;
+                oss << value;
+                m_values[key] = oss.str();
+            }
+        }
+
+        bool hasKey(const std::string& key) const;
+        void removeKey(const std::string& key);
+        void clear();
+        std::vector<std::string> getKeys() const;
+        std::vector<std::string> getKeysInSection(const std::string& section) const;
+
+        void setString(const std::string& key, const std::string& value);
+        std::string getString(const std::string& key, const std::string& defaultValue = "") const;
+        int getInt(const std::string& key, int defaultValue = 0) const;
+        float getFloat(const std::string& key, float defaultValue = 0.0f) const;
+        bool getBool(const std::string& key, bool defaultValue = false) const;
+
+    private:
+        Config() = default;
+        Config(const Config&) = delete;
+        Config& operator=(const Config&) = delete;
+
+        std::string trim(const std::string& str) const;
+
+        std::unordered_map<std::string, std::string> m_values;
+    };
+
+}
