466dfce2fb07b844ebf640d2a6660bcebaaf0969
heheboobes <heheboobes@dev.null>
2022-02-05 10:00:00 +1000

    feat: implement fixed timestep and variable game loop modes

---
+++ engine/core/GameLoop.cpp
@@ -0,0 +1,211 @@
+#include "GameLoop.h"
+#include "Logger.h"
+#include "Platform.h"
+#include <algorithm>
+
+namespace nebula {
+
+    GameLoop::GameLoop() {
+        NEBULA_TRACE("GameLoop created");
+    }
+
+    void GameLoop::setLoopMode(LoopMode mode) {
+        m_mode = mode;
+        NEBULA_INFO("GameLoop mode set to: " +
+            std::string(mode == LoopMode::FIXED ? "FIXED" :
+                        mode == LoopMode::VARIABLE ? "VARIABLE" : "HYBRID"));
+    }
+
+    void GameLoop::setFixedTimestep(float timestep) {
+        if (timestep > 0.0f) {
+            m_fixedTimestep = timestep;
+            NEBULA_INFO("Fixed timestep set to: " + std::to_string(timestep) +
+                       " (" + std::to_string(1.0f / timestep) + " UPS)");
+        }
+    }
+
+    void GameLoop::setMaxFrameRate(float fps) {
+        m_maxFramerate = fps;
+        if (fps > 0.0f) {
+            NEBULA_INFO("Max framerate set to: " + std::to_string(fps) + " FPS");
+        } else {
+            NEBULA_INFO("Max framerate: unlimited");
+        }
+    }
+
+    void GameLoop::start(std::function<void(float)> updateCallback,
+                          std::function<void(float)> renderCallback) {
+        if (m_running) {
+            NEBULA_WARN("GameLoop already running");
+            return;
+        }
+
+        if (!updateCallback || !renderCallback) {
+            NEBULA_ERROR("GameLoop requires both update and render callbacks");
+            return;
+        }
+
+        m_running = true;
+        NEBULA_INFO("GameLoop started (mode: " +
+            std::string(m_mode == LoopMode::FIXED ? "FIXED" :
+                        m_mode == LoopMode::VARIABLE ? "VARIABLE" : "HYBRID") + ")");
+
+        switch (m_mode) {
+            case LoopMode::FIXED:
+                fixedLoop(updateCallback, renderCallback);
+                break;
+            case LoopMode::VARIABLE:
+                variableLoop(updateCallback, renderCallback);
+                break;
+            case LoopMode::HYBRID:
+                hybridLoop(updateCallback, renderCallback);
+                break;
+        }
+    }
+
+    void GameLoop::stop() {
+        m_running = false;
+        NEBULA_INFO("GameLoop stopped");
+    }
+
+    void GameLoop::fixedLoop(std::function<void(float)>& update,
+                              std::function<void(float)>& render) {
+        auto previous = std::chrono::high_resolution_clock::now();
+        float lag = 0.0f;
+
+        while (m_running) {
+            auto current = std::chrono::high_resolution_clock::now();
+            auto elapsed = std::chrono::duration_cast<std::chrono::duration<float>>(current - previous);
+            previous = current;
+
+            lag += elapsed.count();
+
+            int steps = 0;
+            while (lag >= m_fixedTimestep && steps < m_maxUpdateSteps) {
+                auto updateStart = std::chrono::high_resolution_clock::now();
+                update(m_fixedTimestep);
+                auto updateEnd = std::chrono::high_resolution_clock::now();
+                m_updateTime = std::chrono::duration_cast<std::chrono::duration<float>>(updateEnd - updateStart).count();
+
+                lag -= m_fixedTimestep;
+                steps++;
+            }
+
+            if (steps >= m_maxUpdateSteps) {
+                lag = 0.0f;
+            }
+
+            float interpolation = lag / m_fixedTimestep;
+
+            auto renderStart = std::chrono::high_resolution_clock::now();
+            render(interpolation);
+            auto renderEnd = std::chrono::high_resolution_clock::now();
+            m_renderTime = std::chrono::duration_cast<std::chrono::duration<float>>(renderEnd - renderStart).count();
+
+            m_frameTime = std::chrono::duration_cast<std::chrono::duration<float>>(renderEnd - current).count();
+
+            if (m_maxFramerate > 0.0f) {
+                float targetFrameTime = 1.0f / m_maxFramerate;
+                float frameTime = std::chrono::duration_cast<std::chrono::duration<float>>(
+                    std::chrono::high_resolution_clock::now() - current).count();
+                float sleepTime = targetFrameTime - frameTime;
+                if (sleepTime > 0.0f) {
+                    PlatformInfo::getTime();
+                    platformSleep(sleepTime);
+                }
+            }
+        }
+    }
+
+    void GameLoop::variableLoop(std::function<void(float)>& update,
+                                 std::function<void(float)>& render) {
+        auto previous = std::chrono::high_resolution_clock::now();
+
+        while (m_running) {
+            auto current = std::chrono::high_resolution_clock::now();
+            auto elapsed = std::chrono::duration_cast<std::chrono::duration<float>>(current - previous);
+            previous = current;
+
+            float dt = elapsed.count();
+            if (dt > 0.1f) dt = 0.1f;
+
+            {
+                auto updateStart = std::chrono::high_resolution_clock::now();
+                update(dt);
+                auto updateEnd = std::chrono::high_resolution_clock::now();
+                m_updateTime = std::chrono::duration_cast<std::chrono::duration<float>>(updateEnd - updateStart).count();
+            }
+
+            {
+                auto renderStart = std::chrono::high_resolution_clock::now();
+                render(dt);
+                auto renderEnd = std::chrono::high_resolution_clock::now();
+                m_renderTime = std::chrono::duration_cast<std::chrono::duration<float>>(renderEnd - renderStart).count();
+            }
+
+            m_frameTime = std::chrono::duration_cast<std::chrono::duration<float>>(
+                std::chrono::high_resolution_clock::now() - current).count();
+
+            if (m_maxFramerate > 0.0f) {
+                float targetFrameTime = 1.0f / m_maxFramerate;
+                float sleepTime = targetFrameTime - m_frameTime;
+                if (sleepTime > 0.0f) {
+                    platformSleep(sleepTime);
+                }
+            }
+        }
+    }
+
+    void GameLoop::hybridLoop(std::function<void(float)>& update,
+                               std::function<void(float)>& render) {
+        auto previous = std::chrono::high_resolution_clock::now();
+        float accumulator = 0.0f;
+
+        while (m_running) {
+            auto current = std::chrono::high_resolution_clock::now();
+            auto elapsed = std::chrono::duration_cast<std::chrono::duration<float>>(current - previous);
+            previous = current;
+
+            float frameTime = elapsed.count();
+            if (frameTime > 0.1f) frameTime = 0.1f;
+
+            accumulator += frameTime;
+
+            int steps = 0;
+            while (accumulator >= m_fixedTimestep && steps < m_maxUpdateSteps) {
+                auto updateStart = std::chrono::high_resolution_clock::now();
+                update(m_fixedTimestep);
+                auto updateEnd = std::chrono::high_resolution_clock::now();
+                m_updateTime = std::chrono::duration_cast<std::chrono::duration<float>>(updateEnd - updateStart).count();
+
+                accumulator -= m_fixedTimestep;
+                steps++;
+            }
+
+            if (steps >= m_maxUpdateSteps) {
+                accumulator = 0.0f;
+            }
+
+            float interpolation = accumulator / m_fixedTimestep;
+
+            {
+                auto renderStart = std::chrono::high_resolution_clock::now();
+                render(interpolation);
+                auto renderEnd = std::chrono::high_resolution_clock::now();
+                m_renderTime = std::chrono::duration_cast<std::chrono::duration<float>>(renderEnd - renderStart).count();
+            }
+
+            m_frameTime = std::chrono::duration_cast<std::chrono::duration<float>>(
+                std::chrono::high_resolution_clock::now() - current).count();
+
+            if (m_maxFramerate > 0.0f) {
+                float targetFrameTime = 1.0f / m_maxFramerate;
+                float sleepTime = targetFrameTime - m_frameTime;
+                if (sleepTime > 0.0f) {
+                    platformSleep(sleepTime);
+                }
+            }
+        }
+    }
+
+}
