2b83a8f6c6b32184b3eb594b04f0762416027d1c
heheboobes <heheboobes@dev.null>
2022-04-08 12:00:00 +1000

    chore: add audio aggregate header

---
+++ engine/audio/Audio.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "AudioBuffer.h"
+#include "AudioEffect.h"
+#include "AudioEngine.h"
+#include "AudioListener.h"
+#include "MusicPlayer.h"
+#include "Sound.h"
+#include "SoundManager.h"
