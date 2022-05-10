53d2e0590ba225b87476bab05766d31df48b13d6
heheboobes <heheboobes@dev.null>
2022-05-10 12:00:00 +1000

    chore: add physics aggregate header

---
+++ engine/physics/Physics.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "PhysicsMaterial.h"
+#include "Collider.h"
+#include "RigidBody.h"
+#include "Broadphase.h"
+#include "Narrowphase.h"
+#include "CollisionDetector.h"
+#include "Joint.h"
+#include "Raycast.h"
+#include "PhysicsWorld.h"
