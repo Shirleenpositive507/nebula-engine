3cdda87ef653ca0c3cd4c347d3eb03be3a4d83ea
heheboobes <heheboobes@dev.null>
2022-06-08 12:00:00 +1000

    chore: add ECS aggregate header

---
+++ engine/ecs/ECS.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "Component.h"
+#include "ComponentStorage.h"
+#include "Entity.h"
+#include "Archetype.h"
+#include "Query.h"
+#include "System.h"
+#include "EntityManager.h"
