312ebe09215753231a695d24e17755aa04fbe9f6
heheboobes <heheboobes@dev.null>
2022-12-02 12:00:00 +1000

    refactor: implement engine state transitions and cleanup

--- engine/core/Engine.cpp
+++ engine/core/Engine.cpp
@@ -10,16 +10,18 @@ namespace nebula {
     }
 
     Engine::~Engine() {
-        if (m_initialized) {
+        if (m_state != EngineState::Uninitialized) {
             shutdown();
         }
     }
 
     bool Engine::initialize(const std::string& configPath) {
-        if (m_initialized) {
+        if (m_state != EngineState::Uninitialized) {
             Logger::warn("Engine already initialized");
             return true;
         }
+
+        m_state = EngineState::Initializing;
 
         Logger::init("nebula.log");
         Logger::info("Nebula Engine v" + m_version + " initializing...");
@@ -39,6 +41,8 @@ namespace nebula {
         Input::instance();
         Time::instance();
 
+        m_eventBus = std::make_unique<EventBus>();
+
         int width = settings.getWidth();
         int height = settings.getHeight();
         bool fullscreen = settings.getFullscreen();
@@ -47,6 +51,7 @@ namespace nebula {
         m_window = std::make_unique<Window>();
         if (!m_window->create(width, height, "Nebula Engine", fullscreen, aa)) {
             Logger::fatal("Failed to create window");
+            m_state = EngineState::Uninitialized;
             return false;
         }
 
@@ -62,41 +67,44 @@ namespace nebula {
 
         m_gameLoop = std::make_unique<GameLoop>();
 
-        m_initialized = true;
+        m_state = EngineState::Running;
         Logger::info("Engine initialization complete");
         return true;
     }
 
     void Engine::shutdown() {
-        if (!m_initialized) return;
+        if (m_state == EngineState::Uninitialized) return;
 
         Logger::info("Engine shutting down...");
 
-        if (m_running) {
+        if (m_state == EngineState::Running || m_state == EngineState::Paused) {
             stop();
         }
 
+        m_state = EngineState::Stopped;
+
         m_gameLoop.reset();
         m_eventDispatcher.reset();
+        m_eventBus.reset();
         m_window.reset();
 
-        m_initialized = false;
+        m_state = EngineState::Uninitialized;
 
         Logger::shutdown();
     }
 
     void Engine::run() {
-        if (!m_initialized) {
+        if (m_state == EngineState::Uninitialized) {
             Logger::fatal("Cannot run engine before initialize()");
             return;
         }
 
-        if (m_running) {
+        if (m_state == EngineState::Running) {
             Logger::warn("Engine already running");
             return;
         }
 
-        m_running = true;
+        m_state = EngineState::Running;
         Logger::info("Engine entering main loop");
 
         NEBULA_PROFILE_SCOPE("Engine::run");
@@ -105,15 +113,37 @@ namespace nebula {
         auto renderFn = [this](float dt) { render(dt); };
 
         m_gameLoop->start(updateFn, renderFn);
+
+        if (m_state == EngineState::Running) {
+            m_state = EngineState::Stopped;
+        }
     }
 
     void Engine::stop() {
-        if (!m_running) return;
-        m_running = false;
+        if (m_state != EngineState::Running && m_state != EngineState::Paused) return;
+        m_state = EngineState::Stopped;
         if (m_gameLoop) {
             m_gameLoop->stop();
         }
         Logger::info("Engine stopped");
+    }
+
+    void Engine::pause() {
+        if (m_state != EngineState::Running) {
+            Logger::warn("Cannot pause engine in current state");
+            return;
+        }
+        m_state = EngineState::Paused;
+        Logger::info("Engine paused");
+    }
+
+    void Engine::resume() {
+        if (m_state != EngineState::Paused) {
+            Logger::warn("Cannot resume engine in current state");
+            return;
+        }
+        m_state = EngineState::Running;
+        Logger::info("Engine resumed");
     }
 
     void Engine::onEvent(Event& event) {
@@ -122,6 +152,11 @@ namespace nebula {
 
     void Engine::update(float dt) {
         NEBULA_PROFILE_FUNCTION();
+
+        if (m_state == EngineState::Paused) {
+            PlatformInfo::sleep(0.001);
+            return;
+        }
 
         Time::instance().update();
         Input::instance().update();
@@ -139,7 +174,9 @@ namespace nebula {
     void Engine::render(float dt) {
         NEBULA_PROFILE_FUNCTION();
 
-        m_window->clear();
+        if (m_state == EngineState::Paused) return;
+
+        m_window->clear(m_clearColor);
 
         if (m_renderCallback) {
             m_renderCallback(dt);
@@ -185,3 +222,4 @@ namespace nebula {
     }
 
 }
+
