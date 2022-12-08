3155d93ed0c4d1754e19a8cbe826998a95fd8488
heheboobes <heheboobes@dev.null>
2022-12-08 12:00:00 +1000

    feat: implement gamepad input and action mapping

--- engine/core/Input.cpp
+++ engine/core/Input.cpp
@@ -1,4 +1,6 @@
 #include "Input.h"
+#include "Event.h"
+#include "Logger.h"
 #include <SFML/System/Clipboard.hpp>
 #include <cstring>
 
@@ -13,15 +15,13 @@ namespace nebula {
         m_keysPressed.clear();
         m_keysReleased.clear();
 
-        for (auto& [key, down] : m_keysDown) {
-            (void)down;
-        }
-
         m_mousePressed.clear();
         m_mouseReleased.clear();
         m_mouseDeltaX = 0.0f;
         m_mouseDeltaY = 0.0f;
         m_scrollDelta = 0.0f;
+
+        pollGamepads();
     }
 
     bool Input::isKeyDown(sf::Keyboard::Key key) const {
@@ -97,16 +97,209 @@ namespace nebula {
         m_scrollDelta += delta;
     }
 
+    void Input::onTextEntered(uint32_t codepoint) {
+        m_textInputBuffer.push_back(codepoint);
+    }
+
+    std::string Input::getTextInput() {
+        std::string result;
+        for (uint32_t cp : m_textInputBuffer) {
+            if (cp <= 0x7F) {
+                result += static_cast<char>(cp);
+            }
+        }
+        m_textInputBuffer.clear();
+        return result;
+    }
+
+    void Input::clearTextInput() {
+        m_textInputBuffer.clear();
+    }
+
     bool Input::isGamepadConnected(int id) const {
         return sf::Joystick::isConnected(id);
     }
 
+    int Input::getConnectedGamepadCount() const {
+        int count = 0;
+        for (int i = 0; i < sf::Joystick::Count; ++i) {
+            if (sf::Joystick::isConnected(i)) ++count;
+        }
+        return count;
+    }
+
     float Input::getGamepadAxis(int id, sf::Joystick::Axis axis) const {
-        return sf::Joystick::getAxisPosition(id, axis);
+        auto it = m_gamepadStates.find(id);
+        if (it != m_gamepadStates.end() && it->second.connected) {
+            return it->second.axes[axis];
+        }
+        return 0.0f;
     }
 
     bool Input::isGamepadButtonDown(int id, int button) const {
-        return sf::Joystick::isButtonPressed(id, button);
+        auto it = m_gamepadStates.find(id);
+        if (it != m_gamepadStates.end() && it->second.connected) {
+            return it->second.buttons[button];
+        }
+        return false;
+    }
+
+    bool Input::isGamepadButtonPressed(int id, int button) const {
+        auto it = m_gamepadStates.find(id);
+        if (it != m_gamepadStates.end() && it->second.connected) {
+            return it->second.buttonsPressed[button];
+        }
+        return false;
+    }
+
+    bool Input::isGamepadButtonReleased(int id, int button) const {
+        auto it = m_gamepadStates.find(id);
+        if (it != m_gamepadStates.end() && it->second.connected) {
+            return it->second.buttonsReleased[button];
+        }
+        return false;
+    }
+
+    const GamepadState& Input::getGamepadState(int id) const {
+        static GamepadState defaultState;
+        auto it = m_gamepadStates.find(id);
+        if (it != m_gamepadStates.end()) {
+            return it->second;
+        }
+        return defaultState;
+    }
+
+    void Input::pollGamepads() {
+        for (int i = 0; i < sf::Joystick::Count; ++i) {
+            GamepadState& state = m_gamepadStates[i];
+            bool wasConnected = state.connected;
+            state.connected = sf::Joystick::isConnected(i);
+
+            for (int b = 0; b < sf::Joystick::ButtonCount; ++b) {
+                bool wasDown = state.buttons[b];
+                state.buttons[b] = state.connected ? sf::Joystick::isButtonPressed(i, b) : false;
+                state.buttonsPressed[b] = state.connected && !wasDown && state.buttons[b];
+                state.buttonsReleased[b] = state.connected && wasDown && !state.buttons[b];
+            }
+
+            for (int a = 0; a < sf::Joystick::AxisCount; ++a) {
+                state.axes[a] = state.connected ? sf::Joystick::getAxisPosition(i, static_cast<sf::Joystick::Axis>(a)) : 0.0f;
+            }
+
+            if (state.connected != wasConnected) {
+                if (state.connected) {
+                    NEBULA_INFO("Gamepad connected: " + std::to_string(i));
+                } else {
+                    NEBULA_INFO("Gamepad disconnected: " + std::to_string(i));
+                }
+            }
+        }
+    }
+
+    void Input::bindAction(const std::string& name, int primaryKey, int secondaryKey,
+                           int gamepadButton, int mouseButton, float deadZone) {
+        InputAction action;
+        action.name = name;
+        action.primaryKey = primaryKey;
+        action.secondaryKey = secondaryKey;
+        action.gamepadButton = gamepadButton;
+        action.mouseButton = mouseButton;
+        action.deadZone = deadZone;
+        m_actions[name] = action;
+
+        if (!m_contextStack.empty()) {
+            m_contextActions[m_contextStack.top()].push_back(name);
+        }
+    }
+
+    void Input::unbindAction(const std::string& name) {
+        m_actions.erase(name);
+    }
+
+    bool Input::resolveActionKey(const InputAction& action, bool (*keyCheck)(sf::Keyboard::Key),
+                                  bool (*mouseCheck)(sf::Mouse::Button),
+                                  bool (*gamepadCheck)(int, int)) const {
+        if (action.primaryKey >= 0) {
+            sf::Keyboard::Key key = static_cast<sf::Keyboard::Key>(action.primaryKey);
+            if (keyCheck(key)) return true;
+        }
+        if (action.secondaryKey >= 0) {
+            sf::Keyboard::Key key = static_cast<sf::Keyboard::Key>(action.secondaryKey);
+            if (keyCheck(key)) return true;
+        }
+        if (action.mouseButton >= 0) {
+            sf::Mouse::Button btn = static_cast<sf::Mouse::Button>(action.mouseButton);
+            if (mouseCheck(btn)) return true;
+        }
+        if (action.gamepadButton >= 0) {
+            for (int i = 0; i < sf::Joystick::Count; ++i) {
+                if (gamepadCheck(i, action.gamepadButton)) return true;
+            }
+        }
+        return false;
+    }
+
+    bool Input::isActionTriggered(const std::string& name) const {
+        auto it = m_actions.find(name);
+        if (it == m_actions.end()) return false;
+        return resolveActionKey(it->second,
+            [this](sf::Keyboard::Key k) { return isKeyPressed(k); },
+            [this](sf::Mouse::Button b) { return isMouseButtonPressed(b); },
+            [this](int id, int btn) { return isGamepadButtonPressed(id, btn); });
+    }
+
+    bool Input::isActionDown(const std::string& name) const {
+        auto it = m_actions.find(name);
+        if (it == m_actions.end()) return false;
+        return resolveActionKey(it->second,
+            [this](sf::Keyboard::Key k) { return isKeyDown(k); },
+            [this](sf::Mouse::Button b) { return isMouseButtonDown(b); },
+            [this](int id, int btn) { return isGamepadButtonDown(id, btn); });
+    }
+
+    bool Input::isActionReleased(const std::string& name) const {
+        auto it = m_actions.find(name);
+        if (it == m_actions.end()) return false;
+        return resolveActionKey(it->second,
+            [this](sf::Keyboard::Key k) { return isKeyReleased(k); },
+            [this](sf::Mouse::Button b) { return isMouseButtonReleased(b); },
+            [this](int id, int btn) { return isGamepadButtonReleased(id, btn); });
+    }
+
+    void Input::pushContext(const std::string& context) {
+        m_contextStack.push(context);
+        NEBULA_DEBUG("Input context pushed: " + context);
+    }
+
+    void Input::popContext() {
+        if (!m_contextStack.empty()) {
+            std::string top = m_contextStack.top();
+            m_contextStack.pop();
+            NEBULA_DEBUG("Input context popped: " + top);
+        }
+    }
+
+    void Input::setContext(const std::string& context) {
+        while (!m_contextStack.empty()) {
+            m_contextStack.pop();
+        }
+        m_contextStack.push(context);
+        NEBULA_DEBUG("Input context set to: " + context);
+    }
+
+    const std::string& Input::getCurrentContext() const {
+        static std::string defaultContext = "default";
+        if (m_contextStack.empty()) return defaultContext;
+        return m_contextStack.top();
+    }
+
+    bool Input::hasContext(const std::string& context) const {
+        std::stack<std::string> temp = m_contextStack;
+        while (!temp.empty()) {
+            if (temp.top() == context) return true;
+            temp.pop();
+        }
+        return false;
     }
 
     void Input::setClipboardText(const std::string& text) {
@@ -124,3 +317,4 @@ namespace nebula {
     }
 
 }
+
