37ff3368a10253e2a593e53e0e557d4e55870e80
heheboobes <heheboobes@dev.null>
2023-01-15 12:00:00 +1000

    refactor: add render passes and batch statistics

--- engine/graphics/Renderer.h
+++ engine/graphics/Renderer.h
@@ -12,20 +12,54 @@
 #include "RenderTexture.h"
 #include <stack>
 #include <cstddef>
+#include <string>
+#include <functional>
 
 namespace nebula {
     namespace graphics {
+
+        enum class RenderPass {
+            Shadow,
+            Opaque,
+            Transparent,
+            UI,
+            Count
+        };
+
+        enum class BatchMode {
+            Immediate,
+            Batched,
+            Count
+        };
+
+        struct ScopedRenderState {
+            sf::View view;
+            BlendState blend;
+            RenderPass pass;
+            bool active;
+
+            ScopedRenderState() : view(), blend(), pass(RenderPass::Opaque), active(false) {}
+        };
 
         struct FrameStats {
             std::size_t drawCalls;
             std::size_t vertices;
             std::size_t batches;
+            std::size_t stateChanges;
             float frameTime;
             float cpuTime;
 
             FrameStats()
-                : drawCalls(0), vertices(0), batches(0)
+                : drawCalls(0), vertices(0), batches(0), stateChanges(0)
                 , frameTime(0.f), cpuTime(0.f) {}
+        };
+
+        struct RenderPassStats {
+            std::size_t drawCalls;
+            std::size_t vertices;
+            std::string passName;
+
+            RenderPassStats() : drawCalls(0), vertices(0), passName("") {}
         };
 
         class Renderer {
@@ -77,16 +111,43 @@ namespace nebula {
             void setVSync(bool enabled);
             bool isVSync() const;
 
+            void setBatchMode(BatchMode mode);
+            BatchMode getBatchMode() const;
+
+            void beginRenderPass(RenderPass pass);
+            void endRenderPass(RenderPass pass);
+            RenderPass getCurrentRenderPass() const;
+
+            void pushRenderState(const ScopedRenderState& state);
+            void popRenderState();
+            std::size_t getRenderStateStackSize() const;
+
+            void setPassCallback(RenderPass pass, std::function<void()> callback);
+            void clearPassCallbacks();
+
+            RenderPassStats getPassStats(RenderPass pass) const;
+            std::size_t getStateChanges() const;
+            void setBatchFlushCallback(std::function<void()> callback);
+
+            bool isBatchRendering() const;
+
         private:
             sf::RenderWindow* m_window;
             RenderTexture* m_customTarget;
             sf::RenderTarget* m_activeTarget;
 
             FrameStats m_stats;
+            RenderPassStats m_passStats[static_cast<int>(RenderPass::Count)];
             bool m_frameBegan;
             Color m_clearColor;
             BlendState m_blendState;
             sf::FloatRect m_viewport;
+
+            BatchMode m_batchMode;
+            RenderPass m_currentPass;
+            std::stack<ScopedRenderState> m_stateStack;
+            std::function<void()> m_passCallbacks[static_cast<int>(RenderPass::Count)];
+            std::function<void()> m_batchFlushCallback;
 
             std::stack<sf::View> m_viewStack;
             sf::View m_defaultView;
