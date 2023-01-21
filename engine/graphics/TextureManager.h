3a8af45112e52efa87c8c849b7f7b4d7ccdd5f17
heheboobes <heheboobes@dev.null>
2023-01-21 12:00:00 +1000

    feat: add texture atlas and async loading

--- engine/graphics/TextureManager.h
+++ engine/graphics/TextureManager.h
@@ -8,9 +8,48 @@
 #include <memory>
 #include <vector>
 #include <cstddef>
+#include <future>
+#include <functional>
+#include <map>
 
 namespace nebula {
     namespace graphics {
+
+        struct AtlasRegion {
+            std::string name;
+            sf::IntRect rect;
+            bool rotated;
+
+            AtlasRegion() : rect(), rotated(false) {}
+            AtlasRegion(const std::string& n, const sf::IntRect& r, bool rot = false)
+                : name(n), rect(r), rotated(rot) {}
+        };
+
+        struct AtlasPage {
+            std::shared_ptr<sf::Texture> texture;
+            std::vector<AtlasRegion> regions;
+            unsigned int width;
+            unsigned int height;
+            bool packed;
+
+            AtlasPage() : width(0), height(0), packed(false) {}
+        };
+
+        enum class TextureCompression {
+            None,
+            DXT1,
+            DXT3,
+            DXT5,
+            ETC2,
+            ASTC
+        };
+
+        enum class MipmapMode {
+            None,
+            Linear,
+            Nearest,
+            Trilinear
+        };
 
         class TextureManager {
         public:
@@ -37,6 +76,33 @@ namespace nebula {
             void setMaxTextureSize(unsigned int maxSize);
             unsigned int getMaxTextureSize() const;
 
+            // Texture atlas support
+            int createAtlasPage(const std::string& name, unsigned int width, unsigned int height);
+            bool packTextureIntoAtlas(const std::string& atlasName, const std::string& textureName, const std::string& filepath);
+            std::shared_ptr<sf::Texture> getAtlasPage(const std::string& name) const;
+            const AtlasRegion* getAtlasRegion(const std::string& atlasName, const std::string& regionName) const;
+            std::vector<std::string> getAtlasNames() const;
+
+            // Compression detection
+            TextureCompression detectCompression(const std::string& filepath) const;
+            void setCompressionMode(TextureCompression mode);
+            TextureCompression getCompressionMode() const;
+
+            // Async streaming
+            std::future<std::shared_ptr<sf::Texture>> loadTextureAsync(const std::string& name, const std::string& filepath);
+            bool isLoaded(const std::string& name) const;
+            void waitForLoad(const std::string& name);
+
+            // Mipmap generation
+            void setMipmapMode(MipmapMode mode);
+            MipmapMode getMipmapMode() const;
+            void generateMipmaps(const std::string& name);
+            void generateMipmapsAll();
+
+            // Anisotropy filtering
+            void setAnisotropyLevel(unsigned int level);
+            unsigned int getAnisotropyLevel() const;
+
             void releaseAll();
 
         private:
@@ -59,9 +125,13 @@ namespace nebula {
 
             std::unordered_map<std::string, TextureEntry> m_textures;
             std::unordered_map<std::string, RenderTextureEntry> m_renderTextures;
+            std::unordered_map<std::string, AtlasPage> m_atlasPages;
             sf::Texture::FilterMode m_defaultFiltering;
             bool m_texturePacking;
             unsigned int m_maxTextureSize;
+            TextureCompression m_compressionMode;
+            MipmapMode m_mipmapMode;
+            unsigned int m_anisotropyLevel;
         };
 
     }
