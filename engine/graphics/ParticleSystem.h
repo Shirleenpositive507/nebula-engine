7ca7f73922a985af46940dd46da645c1f1ffee88
heheboobes <heheboobes@dev.null>
2023-01-29 12:00:00 +1000

    feat: add particle attractors and GPU particles

--- engine/graphics/ParticleSystem.h
+++ engine/graphics/ParticleSystem.h
@@ -5,13 +5,48 @@
 #include <SFML/Graphics/RenderTarget.hpp>
 #include <SFML/Graphics/RenderStates.hpp>
 #include <SFML/Graphics/PrimitiveType.hpp>
+#include <SFML/System/Vector2.hpp>
 #include <string>
 #include <unordered_map>
 #include <memory>
 #include <vector>
+#include <functional>
+#include <algorithm>
 
 namespace nebula {
     namespace graphics {
+
+        struct ParticleAttractor {
+            sf::Vector2f position;
+            float strength;
+            float radius;
+            bool repeller;
+
+            ParticleAttractor()
+                : position(0.f, 0.f), strength(1.f), radius(100.f), repeller(false) {}
+            ParticleAttractor(const sf::Vector2f& pos, float str, float rad, bool repel = false)
+                : position(pos), strength(str), radius(rad), repeller(repel) {}
+        };
+
+        struct ParticleTrail {
+            std::vector<sf::Vector2f> points;
+            float lifetime;
+            float time;
+            bool enabled;
+
+            ParticleTrail() : lifetime(0.5f), time(0), enabled(false) {}
+        };
+
+        enum class ParticleSortMode {
+            None,
+            BackToFront,
+            FrontToBack
+        };
+
+        enum class ParticleUpdateMode {
+            CPU,
+            GPU
+        };
 
         class ParticleSystem {
         public:
@@ -44,6 +79,37 @@ namespace nebula {
 
             std::vector<std::string> getEmitterNames() const;
 
+            // Attractors and repellers
+            void addAttractor(const ParticleAttractor& attractor);
+            void removeAttractor(std::size_t index);
+            void clearAttractors();
+            std::size_t getAttractorCount() const;
+            const ParticleAttractor& getAttractor(std::size_t index) const;
+
+            // Particle trails
+            void setTrailEnabled(bool enabled);
+            bool isTrailEnabled() const;
+            void setTrailLifetime(float lifetime);
+            float getTrailLifetime() const;
+
+            // Particle sorting
+            void setSortMode(ParticleSortMode mode);
+            ParticleSortMode getSortMode() const;
+
+            // GPU particle support
+            void setUpdateMode(ParticleUpdateMode mode);
+            ParticleUpdateMode getUpdateMode() const;
+            bool isGPUAccelerated() const;
+
+            // Sub-frame emission
+            void setSubFrameEmission(bool enabled);
+            bool isSubFrameEmissionEnabled() const;
+            void setSubFrameSteps(unsigned int steps);
+            unsigned int getSubFrameSteps() const;
+
+            using ParticleRenderCallback = std::function<void(sf::RenderTarget&, const sf::VertexArray&)>;
+            void setRenderCallback(ParticleRenderCallback callback);
+
         private:
             std::unordered_map<std::string, std::shared_ptr<ParticleEmitter>> m_emitters;
             sf::VertexArray m_vertexArray;
@@ -51,7 +117,20 @@ namespace nebula {
             sf::Vector2f m_globalGravity;
             sf::Vector2f m_globalWind;
 
+            std::vector<ParticleAttractor> m_attractors;
+            std::vector<ParticleTrail> m_trails;
+            ParticleSortMode m_sortMode;
+            ParticleUpdateMode m_updateMode;
+            bool m_trailEnabled;
+            float m_trailLifetime;
+            bool m_subFrameEmission;
+            unsigned int m_subFrameSteps;
+            ParticleRenderCallback m_renderCallback;
+
             void buildVertexArray();
+            void applyAttractors(float dt);
+            void sortParticles();
+            void updateTrails(float dt);
         };
 
     }
