8ec978a8bcddb7ba2018ff11b3de54791bd5f908
heheboobes <heheboobes@dev.null>
2023-02-07 12:00:00 +1000

    feat: add sound priority and 3D occlusion

--- engine/audio/SoundManager.h
+++ engine/audio/SoundManager.h
@@ -2,11 +2,13 @@
 
 #include "Sound.h"
 #include "AudioBuffer.h"
+#include "math/Vector3.h"
 
 #include <memory>
 #include <string>
 #include <unordered_map>
 #include <vector>
+#include <algorithm>
 
 namespace nebula {
 namespace audio {
@@ -18,11 +20,32 @@ enum class SoundGroup {
     UI
 };
 
+struct OcclusionParams {
+    float occlusionFactor;
+    float obstructionFactor;
+    float lowPassCutoff;
+    float volumeAttenuation;
+
+    OcclusionParams()
+        : occlusionFactor(0.0f)
+        , obstructionFactor(0.0f)
+        , lowPassCutoff(22000.0f)
+        , volumeAttenuation(1.0f) {}
+};
+
 struct SoundInstance {
     std::unique_ptr<Sound> sound;
     std::string name;
     SoundGroup group;
     bool inUse = false;
+    int priority = 0;
+    float audibleRange = 100.0f;
+    Vector3f position3D;
+    bool is3D = false;
+    OcclusionParams occlusion;
+    float currentOcclusionTimer = 0.0f;
+
+    SoundInstance() : priority(0) {}
 };
 
 class SoundManager {
@@ -37,6 +60,13 @@ public:
                                      float pitch = 1.f,
                                      bool loop = false);
 
+    std::shared_ptr<Sound> playSound3D(const std::string& name,
+                                        const Vector3f& position,
+                                        float volume = 100.f,
+                                        float pitch = 1.f,
+                                        bool loop = false,
+                                        int priority = 0);
+
     void stopSound(const std::string& name);
     void stopAll();
     void pauseAll();
@@ -45,6 +75,15 @@ public:
     void setVolume(const std::string& name, float volume);
     void setGlobalVolume(float volume);
     void setGroupVolume(SoundGroup group, float volume);
+    void setGlobalPitch(float pitch);
+
+    void setSoundPriority(const std::string& name, int priority);
+    void setSoundAudibleRange(const std::string& name, float range);
+    void setSoundPosition(const std::string& name, const Vector3f& position);
+    Vector3f getSoundPosition(const std::string& name) const;
+    void setSoundOcclusion(const std::string& name, float occlusionFactor, float obstructionFactor);
+
+    void preAllocateInstances(std::size_t count);
 
     std::shared_ptr<Sound> getSound(const std::string& name) const;
     bool soundExists(const std::string& name) const;
@@ -65,12 +104,15 @@ private:
 
     SoundInstance* acquireInstance();
     void releaseInstance(SoundInstance* instance);
+    void cullLowPrioritySounds();
+    OcclusionParams calculateOcclusion(const Vector3f& soundPos) const;
 
     AudioBufferManager m_bufferManager;
     std::vector<SoundInstance> m_pool;
     std::unordered_map<std::string, SoundInstance*> m_activeSounds;
     std::unordered_map<SoundGroup, float> m_groupVolumes;
     float m_globalVolume = 1.f;
+    float m_globalPitch = 1.f;
     std::size_t m_maxConcurrent = 64;
     bool m_initialized = false;
 };
