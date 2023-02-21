08ebfa2ae6c442e3683fb3661d5cc54b00ac1e82
heheboobes <heheboobes@dev.null>
2023-02-21 12:00:00 +1000

    feat: add dynamic broadphase tree (DBVT)

--- engine/physics/Broadphase.h
+++ engine/physics/Broadphase.h
@@ -1,8 +1,12 @@
 #pragma once
 #include <vector>
 #include <unordered_map>
+#include <unordered_set>
 #include <utility>
 #include <functional>
+#include <memory>
+#include <algorithm>
+#include <mutex>
 #include "core/Types.h"
 #include "math/Vector2.h"
 #include "math/Rect.h"
@@ -31,14 +35,52 @@ struct ColliderPairHash {
     }
 };
 
+struct DBVTNode {
+    Rectf aabb;
+    RigidBody* body;
+    std::unique_ptr<DBVTNode> left;
+    std::unique_ptr<DBVTNode> right;
+    DBVTNode* parent;
+    i32 height;
+    bool isLeaf;
+
+    DBVTNode()
+        : body(nullptr)
+        , parent(nullptr)
+        , height(1)
+        , isLeaf(true) {}
+};
+
+struct BroadphaseStats {
+    std::size_t totalPairs;
+    std::size_t potentialPairs;
+    std::size_t cellCount;
+    std::size_t dbvtNodes;
+    i32 dbvtHeight;
+    f32 averagePairsPerCell;
+    std::size_t queryTime;
+
+    BroadphaseStats()
+        : totalPairs(0)
+        , potentialPairs(0)
+        , cellCount(0)
+        , dbvtNodes(0)
+        , dbvtHeight(0)
+        , averagePairsPerCell(0.0f)
+        , queryTime(0) {}
+};
+
 class Broadphase {
 public:
     f32 cellSize;
     std::unordered_map<i64, std::vector<RigidBody*>> grid;
     std::unordered_map<RigidBody*, i64> bodyCells;
+    bool useDBVT;
+    bool useMultiThreading;
 
-    Broadphase() : cellSize(2.0f) {}
-    explicit Broadphase(f32 cellSize) : cellSize(cellSize) {}
+    Broadphase() : cellSize(2.0f), useDBVT(false), useMultiThreading(false) {}
+    explicit Broadphase(f32 cellSize)
+        : cellSize(cellSize), useDBVT(false), useMultiThreading(false) {}
 
     void insert(RigidBody* body);
     void remove(RigidBody* body);
@@ -48,6 +90,14 @@ public:
     std::vector<RigidBody*> queryPoint(const Vector2f& point) const;
     std::vector<ColliderPair> queryRay(const Vector2f& origin, const Vector2f& direction, f32 maxDist) const;
     std::vector<ColliderPair> getPotentialCollisions() const;
+
+    void autoConfigureCellSize();
+    void setUseDBVT(bool enabled);
+    void setUseMultiThreading(bool enabled);
+
+    BroadphaseStats getStats() const;
+
+    void rebuildDBVT();
 
 private:
     i64 hashCell(i32 cx, i32 cy) const {
@@ -61,6 +111,23 @@ private:
     }
 
     void getCellRange(const Rectf& rect, i32& minCX, i32& minCY, i32& maxCX, i32& maxCY) const;
+
+    std::unique_ptr<DBVTNode> m_dbvtRoot;
+    std::unordered_map<RigidBody*, DBVTNode*> m_dbvtLeaves;
+    mutable std::mutex m_mutex;
+
+    DBVTNode* createLeaf(RigidBody* body);
+    void removeLeaf(DBVTNode* leaf);
+    DBVTNode* insertLeaf(DBVTNode* root, DBVTNode* leaf);
+    DBVTNode* removeLeafFromTree(DBVTNode* root, DBVTNode* leaf);
+    void updateDBVT(RigidBody* body);
+    void queryDBVT(const Rectf& rect, std::vector<ColliderPair>& pairs) const;
+    void queryDBVTRecursive(DBVTNode* node, const Rectf& rect, std::vector<ColliderPair>& pairs) const;
+    void syncDBVTToGrid();
+    i32 getHeight(DBVTNode* node) const;
+    i32 getBalance(DBVTNode* node) const;
+    f32 getPerimeter(const Rectf& rect) const;
+    Rectf combineRect(const Rectf& a, const Rectf& b) const;
 };
 
 } // namespace nebula
