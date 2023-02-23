c4375e2e6f7be71c8ffc3b566a021822d24fb248
heheboobes <heheboobes@dev.null>
2023-02-23 12:00:00 +1000

    feat: add GJK EPA and capsule collision

--- engine/physics/Narrowphase.h
+++ engine/physics/Narrowphase.h
@@ -2,6 +2,7 @@
 #include <vector>
 #include <cmath>
 #include <algorithm>
+#include <limits>
 #include "core/Types.h"
 #include "math/Vector2.h"
 #include "math/Rect.h"
@@ -28,6 +29,53 @@ struct CollisionInfo {
     CollisionInfo() : colliding(false), penetration(0), normal(0, 0) {}
 };
 
+struct ManifoldPoint {
+    Vector2f localPointA;
+    Vector2f localPointB;
+    Vector2f worldPoint;
+    Vector2f normal;
+    f32 penetration;
+    f32 normalImpulse;
+    f32 tangentImpulse;
+    f32 warmStartFactor;
+    bool active;
+
+    ManifoldPoint()
+        : penetration(0)
+        , normalImpulse(0)
+        , tangentImpulse(0)
+        , warmStartFactor(0.95f)
+        , active(true) {}
+};
+
+struct TimeOfImpact {
+    bool colliding;
+    f32 toi;
+    Vector2f point;
+    Vector2f normal;
+
+    TimeOfImpact() : colliding(false), toi(1.0f) {}
+};
+
+struct SimplexVertex {
+    Vector2f pointA;
+    Vector2f pointB;
+    Vector2f diff;
+    Vector2f barycentric;
+
+    SimplexVertex() : pointA(0, 0), pointB(0, 0), diff(0, 0), barycentric(0, 0) {}
+};
+
+struct Simplex {
+    std::vector<SimplexVertex> vertices;
+
+    void clear() { vertices.clear(); }
+    void add(const SimplexVertex& v) { vertices.push_back(v); }
+    std::size_t size() const { return vertices.size(); }
+    SimplexVertex& operator[](std::size_t i) { return vertices[i]; }
+    const SimplexVertex& operator[](std::size_t i) const { return vertices[i]; }
+};
+
 class Narrowphase {
 public:
     static CollisionInfo AABBvsAABB(const BoxCollider& a, const Vector2f& posA,
@@ -43,6 +91,27 @@ public:
     static CollisionInfo AABBvsPolygon(const BoxCollider& box, const Vector2f& boxPos,
                                         const PolygonCollider& poly, const Vector2f& polyPos, f32 polyRot);
 
+    static CollisionInfo GJK(const std::vector<Vector2f>& vertsA, const Vector2f& posA, f32 rotA,
+                              const std::vector<Vector2f>& vertsB, const Vector2f& posB, f32 rotB);
+    static CollisionInfo GJK_EPA(const std::vector<Vector2f>& vertsA, const Vector2f& posA, f32 rotA,
+                                  const std::vector<Vector2f>& vertsB, const Vector2f& posB, f32 rotB);
+
+    static CollisionInfo CapsuleCollision(const Vector2f& capA1, const Vector2f& capA2, f32 radiusA,
+                                            const Vector2f& capB1, const Vector2f& capB2, f32 radiusB);
+    static CollisionInfo CapsulevsAABB(const Vector2f& cap1, const Vector2f& cap2, f32 radius,
+                                        const BoxCollider& box, const Vector2f& boxPos);
+    static CollisionInfo CapsulevsCircle(const Vector2f& cap1, const Vector2f& cap2, f32 radius,
+                                          const CircleCollider& circle, const Vector2f& circlePos);
+    static CollisionInfo CapsulevsPolygon(const Vector2f& cap1, const Vector2f& cap2, f32 radius,
+                                           const PolygonCollider& poly, const Vector2f& polyPos, f32 polyRot);
+
+    static TimeOfImpact computeTOI(const std::vector<Vector2f>& vertsA, const Vector2f& posA, f32 rotA,
+                                    const Vector2f& velA, f32 angVelA,
+                                    const std::vector<Vector2f>& vertsB, const Vector2f& posB, f32 rotB,
+                                    const Vector2f& velB, f32 angVelB, f32 dt);
+
+    static void reduceManifold(std::vector<ManifoldPoint>& manifold, std::size_t maxPoints = 4);
+
 private:
     static std::vector<Vector2f> getTransformedVertices(const PolygonCollider& poly,
                                                          const Vector2f& pos, f32 rot);
@@ -52,6 +121,17 @@ private:
                         f32& min, f32& max);
     static Vector2f getClosestPointOnPolygon(const Vector2f& point,
                                               const std::vector<Vector2f>& vertices);
+
+    static Vector2f support(const std::vector<Vector2f>& vertsA, const Vector2f& posA, f32 rotA,
+                             const std::vector<Vector2f>& vertsB, const Vector2f& posB, f32 rotB,
+                             const Vector2f& dir);
+    static Vector2f supportLocal(const std::vector<Vector2f>& verts, const Vector2f& dir);
+    static bool solveSimplex2(Simplex& simplex, Vector2f& dir);
+    static bool solveSimplex3(Simplex& simplex, Vector2f& dir);
+
+    static std::vector<Vector2f> buildEdgeList(const std::vector<Vector2f>& polytope);
+    static Vector2f closestPointOnSegment(const Vector2f& a, const Vector2f& b, const Vector2f& p);
+    static f32 closestPointOnSegmentT(const Vector2f& a, const Vector2f& b, const Vector2f& p);
 };
 
 } // namespace nebula
