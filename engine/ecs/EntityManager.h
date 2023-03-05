e75d1213aeeb625f48f7832bd18fa051661259f7
heheboobes <heheboobes@users.noreply.github.com>
2023-03-05 12:00:00 +1000

    feat: add entity cloning and tagging

--- engine/ecs/EntityManager.h
+++ engine/ecs/EntityManager.h
@@ -1,10 +1,12 @@
 #pragma once
 #include <vector>
 #include <unordered_map>
+#include <unordered_set>
 #include <memory>
 #include <functional>
 #include <typeindex>
 #include <cassert>
+#include <string>
 #include "Entity.h"
 #include "Component.h"
 #include "ComponentStorage.h"
@@ -12,6 +14,11 @@
 #include "Query.h"
 
 namespace nebula {
+
+struct EntityGroup {
+    std::vector<ComponentType> requiredComponents;
+    std::string name;
+};
 
 class EntityManager {
 public:
@@ -33,6 +40,21 @@ public:
     u32 getActiveEntityCount() const { return mActiveCount; }
 
     const std::vector<Entity>& getActiveEntities() const { return mActiveEntities; }
+
+    Entity cloneEntity(Entity entity);
+
+    void tagEntity(Entity entity, const std::string& tag);
+    void untagEntity(Entity entity, const std::string& tag);
+    bool hasTag(Entity entity, const std::string& tag) const;
+    std::vector<Entity> getEntitiesWithTag(const std::string& tag) const;
+    std::vector<std::string> getEntityTags(Entity entity) const;
+
+    void setEntityParent(Entity entity, Entity parent);
+    Entity getEntityParent(Entity entity) const;
+    std::vector<Entity> getEntityChildren(Entity entity) const;
+
+    void queryEntityGroup(const EntityGroup& group,
+        std::function<void(Entity)> callback);
 
     template <typename T, typename... Args>
     T& addComponent(Entity entity, Args&&... args);
@@ -71,6 +93,11 @@ private:
         u32 archetypeIndex;
     };
 
+    struct EntityHierarchy {
+        Entity parent;
+        std::vector<Entity> children;
+    };
+
     u64 generateId();
     void recycleId(u64 id);
     void migrateEntity(Entity entity, Archetype* newArchetype);
@@ -94,6 +121,11 @@ private:
     u32 mActiveCount;
 
     SystemManager* mSystemManager;
+
+    std::unordered_map<u64, std::vector<std::string>> mEntityTags;
+    std::unordered_map<std::string, std::vector<Entity>> mTagIndex;
+
+    std::unordered_map<u64, EntityHierarchy> mHierarchy;
 };
 
 }
