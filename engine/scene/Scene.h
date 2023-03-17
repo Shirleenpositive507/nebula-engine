1bd6cdbf7cc0a1d23448fd99cd346a241bb53493
heheboobes <heheboobes@users.noreply.github.com>
2023-03-17 12:00:00 +1000

    feat: add scene render settings and fog

--- engine/scene/Scene.h
+++ engine/scene/Scene.h
@@ -5,6 +5,9 @@
 #include <vector>
 #include <unordered_map>
 #include <functional>
+#include <atomic>
+#include <thread>
+#include <mutex>
 
 #include "engine/scene/SceneNode.h"
 #include "engine/core/EntityManager.h"
@@ -17,9 +20,61 @@
 namespace engine {
 namespace scene {
 
+struct FogSettings {
+    bool enabled;
+    Color color;
+    float density;
+    float startDistance;
+    float endDistance;
+
+    FogSettings()
+        : enabled(false)
+        , color(0.5f, 0.5f, 0.5f, 1.0f)
+        , density(0.01f)
+        , startDistance(10.0f)
+        , endDistance(100.0f) {}
+};
+
+struct RenderSettings {
+    Color ambientColor;
+    Color backgroundColor;
+    FogSettings fog;
+    bool enableShadows;
+    bool enablePostProcessing;
+
+    RenderSettings()
+        : ambientColor(0.2f, 0.2f, 0.2f, 1.0f)
+        , backgroundColor(0.0f, 0.0f, 0.0f, 1.0f)
+        , enableShadows(true)
+        , enablePostProcessing(false) {}
+};
+
+struct LightingSetup {
+    std::vector<Vector3> directionalLights;
+    std::vector<Vector3> pointLights;
+    std::vector<Color> lightColors;
+    float globalIllumination;
+    float ambientIntensity;
+
+    LightingSetup()
+        : globalIllumination(0.3f)
+        , ambientIntensity(1.0f) {}
+};
+
+struct PreloadTask {
+    std::string resourcePath;
+    std::function<bool()> loadFunction;
+    std::function<void(bool)> onComplete;
+    bool completed;
+    bool success;
+
+    PreloadTask() : completed(false), success(false) {}
+};
+
 class Scene {
 public:
     using Ptr = std::shared_ptr<Scene>;
+    using PreloadCallback = std::function<void(float)>;
 
     explicit Scene(const std::string& name = "UntitledScene");
     virtual ~Scene();
@@ -45,12 +100,24 @@ public:
     void setCamera(std::shared_ptr<renderer::Camera> camera) { m_camera = camera; }
     std::shared_ptr<renderer::Camera> getCamera() { return m_camera; }
 
-    void setAmbientColor(const Color& color) { m_ambientColor = color; }
-    const Color& getAmbientColor() const { return m_ambientColor; }
+    void setAmbientColor(const Color& color) { m_renderSettings.ambientColor = color; }
+    const Color& getAmbientColor() const { return m_renderSettings.ambientColor; }
     void setGravity(const Vector3& gravity) { m_gravity = gravity; }
     const Vector3& getGravity() const { return m_gravity; }
-    void setBackgroundColor(const Color& color) { m_backgroundColor = color; }
-    const Color& getBackgroundColor() const { return m_backgroundColor; }
+    void setBackgroundColor(const Color& color) { m_renderSettings.backgroundColor = color; }
+    const Color& getBackgroundColor() const { return m_renderSettings.backgroundColor; }
+
+    RenderSettings& getRenderSettings() { return m_renderSettings; }
+    const RenderSettings& getRenderSettings() const { return m_renderSettings; }
+
+    FogSettings& getFogSettings() { return m_renderSettings.fog; }
+    const FogSettings& getFogSettings() const { return m_renderSettings.fog; }
+
+    LightingSetup& getLightingSetup() { return m_lighting; }
+    const LightingSetup& getLightingSetup() const { return m_lighting; }
+
+    void addDirectionalLight(const Vector3& direction, const Color& color);
+    void addPointLight(const Vector3& position, const Color& color);
 
     struct LayerInfo {
         std::string name;
@@ -72,6 +139,16 @@ public:
     virtual void update(float dt);
     virtual void render(float dt);
 
+    void addPreloadTask(const std::string& path, std::function<bool()> loader,
+                        std::function<void(bool)> onComplete = nullptr);
+    void startPreload(PreloadCallback progressCallback = nullptr);
+    bool isPreloading() const { return m_preloading; }
+    float getPreloadProgress() const;
+    bool isPreloadComplete() const { return m_preloadComplete; }
+
+    void onEvent(const std::string& eventName, std::function<void(const void*)> handler);
+    void dispatchEvent(const std::string& eventName, const void* data = nullptr);
+
     uint32_t createEntity();
     void destroyEntity(uint32_t entityId);
 
@@ -82,13 +159,25 @@ protected:
     EventDispatcher m_eventDispatcher;
     std::shared_ptr<physics::PhysicsWorld> m_physicsWorld;
     std::shared_ptr<renderer::Camera> m_camera;
-    Color m_ambientColor;
     Vector3 m_gravity;
-    Color m_backgroundColor;
     std::unordered_map<int, LayerInfo> m_layers;
     int m_nextLayerId;
 
+    RenderSettings m_renderSettings;
+    LightingSetup m_lighting;
+
+    std::vector<PreloadTask> m_preloadTasks;
+    std::atomic<bool> m_preloading;
+    std::atomic<bool> m_preloadComplete;
+    std::atomic<float> m_preloadProgress;
+    std::thread m_preloadThread;
+    std::mutex m_preloadMutex;
+    PreloadCallback m_preloadCallback;
+
+    std::unordered_map<std::string, std::vector<std::function<void(const void*)>>> m_eventHandlers;
+
     void findNodeByEntityRecursive(SceneNode::Ptr node, uint32_t entityId, SceneNode::Ptr& result) const;
+    void preloadThreadFunc();
 };
 
 } // namespace scene
