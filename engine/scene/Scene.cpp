239a4c88fadbecf217c08db200adc0be0a0f008d
heheboobes <heheboobes@users.noreply.github.com>
2023-03-18 12:00:00 +1000

    feat: implement scene preloader and fog rendering

--- engine/scene/Scene.cpp
+++ engine/scene/Scene.cpp
@@ -6,10 +6,11 @@ namespace scene {
 Scene::Scene(const std::string& name)
     : m_name(name)
     , m_rootNode(std::make_shared<SceneNode>("root"))
-    , m_ambientColor(0.2f, 0.2f, 0.2f, 1.0f)
     , m_gravity(0.0f, -9.81f, 0.0f)
-    , m_backgroundColor(0.0f, 0.0f, 0.0f, 1.0f)
     , m_nextLayerId(0)
+    , m_preloading(false)
+    , m_preloadComplete(false)
+    , m_preloadProgress(0.0f)
 {
     addLayer("default");
 }
@@ -126,9 +127,101 @@ void Scene::destroyEntity(uint32_t entityId)
 {
     auto node = findNodeByEntity(entityId);
     if (node) {
-        node->getParent().lock()->removeChild(node);
+        auto parent = node->getParent().lock();
+        if (parent) {
+            parent->removeChild(node);
+        }
     }
     m_entityManager.destroy(entityId);
+}
+
+void Scene::addDirectionalLight(const Vector3& direction, const Color& color)
+{
+    m_lighting.directionalLights.push_back(direction);
+    m_lighting.lightColors.push_back(color);
+}
+
+void Scene::addPointLight(const Vector3& position, const Color& color)
+{
+    m_lighting.pointLights.push_back(position);
+    m_lighting.lightColors.push_back(color);
+}
+
+void Scene::addPreloadTask(const std::string& path, std::function<bool()> loader,
+                           std::function<void(bool)> onComplete)
+{
+    PreloadTask task;
+    task.resourcePath = path;
+    task.loadFunction = loader;
+    task.onComplete = onComplete;
+    task.completed = false;
+    task.success = false;
+    m_preloadTasks.push_back(task);
+}
+
+void Scene::startPreload(PreloadCallback progressCallback)
+{
+    if (m_preloading) return;
+
+    m_preloadCallback = progressCallback;
+    m_preloading = true;
+    m_preloadComplete = false;
+    m_preloadProgress = 0.0f;
+
+    m_preloadThread = std::thread([this]() {
+        preloadThreadFunc();
+    });
+    m_preloadThread.detach();
+}
+
+float Scene::getPreloadProgress() const
+{
+    return m_preloadProgress.load();
+}
+
+void Scene::onEvent(const std::string& eventName, std::function<void(const void*)> handler)
+{
+    m_eventHandlers[eventName].push_back(handler);
+}
+
+void Scene::dispatchEvent(const std::string& eventName, const void* data)
+{
+    auto it = m_eventHandlers.find(eventName);
+    if (it != m_eventHandlers.end()) {
+        for (auto& handler : it->second) {
+            handler(data);
+        }
+    }
+}
+
+void Scene::preloadThreadFunc()
+{
+    size_t total = m_preloadTasks.size();
+    if (total == 0) {
+        m_preloading = false;
+        m_preloadComplete = true;
+        m_preloadProgress = 1.0f;
+        return;
+    }
+
+    for (size_t i = 0; i < total; ++i) {
+        {
+            std::lock_guard<std::mutex> lock(m_preloadMutex);
+            if (m_preloadTasks[i].loadFunction) {
+                m_preloadTasks[i].success = m_preloadTasks[i].loadFunction();
+            }
+            m_preloadTasks[i].completed = true;
+        }
+
+        m_preloadProgress = static_cast<float>(i + 1) / static_cast<float>(total);
+
+        if (m_preloadCallback) {
+            m_preloadCallback(m_preloadProgress);
+        }
+    }
+
+    m_preloading = false;
+    m_preloadComplete = true;
 }
 
 } // namespace scene
