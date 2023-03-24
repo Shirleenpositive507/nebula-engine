7f4bc79aefe8a8d05e75c613ebf3d10e0487a311
heheboobes <heheboobes@users.noreply.github.com>
2023-03-24 12:00:00 +1000

    feat: add binary scene format and versioning

--- engine/scene/SceneSerializer.h
+++ engine/scene/SceneSerializer.h
@@ -2,6 +2,9 @@
 
 #include <memory>
 #include <string>
+#include <vector>
+#include <cstdint>
+#include <fstream>
 #include <unordered_map>
 
 #include "engine/scene/Scene.h"
@@ -14,8 +17,36 @@
 namespace engine {
 namespace scene {
 
+struct BinarySceneHeader {
+    uint32_t magic;
+    uint32_t versionMajor;
+    uint32_t versionMinor;
+    uint32_t sceneNameLength;
+    uint64_t nodeCount;
+    uint64_t dataSize;
+    uint64_t thumbnailOffset;
+    uint64_t thumbnailSize;
+};
+
+struct SceneDependencyInfo {
+    std::string sceneName;
+    std::string filePath;
+    bool required;
+};
+
+struct ThumbnailData {
+    uint32_t width;
+    uint32_t height;
+    uint32_t channels;
+    std::vector<uint8_t> pixels;
+};
+
 class SceneSerializer {
 public:
+    static constexpr uint32_t BINARY_MAGIC = 0x4E42494E;
+    static constexpr uint32_t CURRENT_VERSION_MAJOR = 1;
+    static constexpr uint32_t CURRENT_VERSION_MINOR = 0;
+
     SceneSerializer();
     ~SceneSerializer();
 
@@ -39,6 +70,16 @@ public:
     SceneNode::Ptr loadPrefab(const std::string& path);
     SceneNode::Ptr instantiatePrefab(const std::string& path, Scene::Ptr scene);
 
+    std::vector<SceneDependencyInfo> getSceneDependencies(const std::string& path) const;
+    void setSceneDependencies(const std::string& path, const std::vector<SceneDependencyInfo>& deps);
+
+    bool generateThumbnail(const std::string& scenePath, const std::string& outputPath,
+                           uint32_t width, uint32_t height);
+    ThumbnailData loadThumbnail(const std::string& scenePath) const;
+
+    bool migrateSceneFormat(const std::string& path, uint32_t fromMajor, uint32_t fromMinor);
+    uint32_t getSerializedVersion(const std::string& path) const;
+
 private:
     struct PrefabTemplate {
         std::string name;
@@ -46,6 +87,7 @@ private:
     };
 
     std::unordered_map<std::string, PrefabTemplate> m_prefabCache;
+    std::unordered_map<std::string, std::vector<SceneDependencyInfo>> m_dependencyCache;
 
     std::string serializeNodeRecursive(SceneNode::Ptr node, int indent);
     SceneNode::Ptr deserializeNodeRecursive(const std::string& json, Scene::Ptr scene);
@@ -58,6 +100,9 @@ private:
     Quaternion deserializeQuaternion(const std::string& str) const;
     std::string serializeColor(const Color& c) const;
     Color deserializeColor(const std::string& str) const;
+
+    bool writeBinaryHeader(std::ofstream& file, const BinarySceneHeader& header) const;
+    BinarySceneHeader readBinaryHeader(std::ifstream& file) const;
 };
 
 } // namespace scene
