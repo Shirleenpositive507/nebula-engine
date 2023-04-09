91b433ce00df9a7c51a5091e16343b2a7ad3b1fc
heheboobes <heheboobes@example.com>
2023-04-09 12:00:00 +1000

    feat: add slider tick marks and fill bar

--- engine/ui/UISlider.h
+++ engine/ui/UISlider.h
@@ -20,14 +20,28 @@ namespace nebula {
             void setValue(float value);
             float getValue() const;
             void setRange(float min, float max);
+            float getMinValue() const;
+            float getMaxValue() const;
             void setStep(float step);
+            float getStep() const;
             void setOrientation(SliderOrientation orientation);
+            SliderOrientation getOrientation() const;
             float getNormalizedValue() const;
 
             void setTrackTexture(const std::shared_ptr<sf::Texture>& texture);
             void setThumbTexture(const std::shared_ptr<sf::Texture>& texture);
             void setTrackColor(const graphics::Color& color);
             void setThumbColor(const graphics::Color& color);
+
+            void setFillColor(const graphics::Color& color);
+            void setShowFill(bool show);
+
+            void setTickMarks(bool show, int count = 10);
+            bool hasTickMarks() const;
+            int getTickMarkCount() const;
+
+            void setSnapToStep(bool snap);
+            bool isSnapToStep() const;
 
             void onRender(sf::RenderTarget& target, sf::RenderStates states) override;
             void onUpdate(float dt) override;
@@ -53,11 +67,17 @@ namespace nebula {
             graphics::Color m_thumbColor;
 
             sf::RectangleShape m_track;
+            sf::RectangleShape m_fillBar;
             sf::RectangleShape m_thumbRect;
             sf::CircleShape m_thumbCircle;
 
             bool m_dragging;
             float m_thumbSize;
+
+            graphics::Color m_fillColor;
+            bool m_showFill;
+
+            bool m_snapToStep;
 
             sf::FloatRect getThumbBounds() const;
             void updateThumbPosition();
