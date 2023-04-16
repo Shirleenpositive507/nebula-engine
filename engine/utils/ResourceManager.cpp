1afe6daef36a4ccaa7175b6f94055aed37f03337
heheboobes <heheboobes@example.com>
2023-04-16 12:00:00 +1000

    feat: implement resource hot-reload system

--- engine/utils/ResourceManager.cpp
+++ engine/utils/ResourceManager.cpp
@@ -1,3 +1,35 @@
 // ResourceManager is primarily template-based and defined in the header.
 // This file exists only to satisfy the build system.
+// Implementation details for hot-reload watching and async queue management
+// are handled through the template methods defined in ResourceManager.h.
 #include "ResourceManager.h"
+#include "FileSystem.h"
+#include <thread>
+#include <chrono>
+
+namespace nebula {
+
+    static FileWatcher s_resourceWatcher;
+
+    void startResourceWatcher(std::function<void(const std::string&)> callback) {
+        s_resourceWatcher.setCallback(std::move(callback));
+    }
+
+    void addResourceWatchPath(const std::string& path) {
+        s_resourceWatcher.watch(path);
+    }
+
+    void pollResourceWatcher() {
+        s_resourceWatcher.poll();
+    }
+
+    void startBackgroundReloadThread(std::function<void()> checkFunc, int intervalMs) {
+        std::thread([checkFunc = std::move(checkFunc), intervalMs]() {
+            while (true) {
+                std::this_thread::sleep_for(std::chrono::milliseconds(intervalMs));
+                checkFunc();
+            }
+        }).detach();
+    }
+
+}
