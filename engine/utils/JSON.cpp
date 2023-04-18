9a05b958eb80292c9f9a3f9ca3ce7d1d32e52a7c
heheboobes <heheboobes@example.com>
2023-04-18 12:00:00 +1000

    feat: implement JSON schema and path queries

--- engine/utils/JSON.cpp
+++ engine/utils/JSON.cpp
@@ -2,6 +2,8 @@
 #include <sstream>
 #include <iomanip>
 #include <cctype>
+#include <algorithm>
+#include <regex>
 
 namespace nebula {
 
@@ -363,6 +365,389 @@ namespace nebula {
     JSONValue JSONValue::parseStream(std::istream& stream) {
         std::string json((std::istreambuf_iterator<char>(stream)), std::istreambuf_iterator<char>());
         return parse(json);
+    }
+
+    JSONValue JSONValue::query(const JSONValue& root, const std::string& path) {
+        if (path.empty() || path == "$") return root;
+
+        std::string p = path;
+        if (p[0] == '$') {
+            if (p.size() == 1) return root;
+            p = p.substr(1);
+        }
+        if (p[0] == '.') p = p.substr(1);
+
+        const JSONValue* current = &root;
+        std::string segment;
+        std::istringstream stream(p);
+
+        while (std::getline(stream, segment, '.')) {
+            if (segment.empty()) continue;
+
+            size_t bracketPos = segment.find('[');
+            if (bracketPos != std::string::npos) {
+                std::string key = segment.substr(0, bracketPos);
+                if (!key.empty()) {
+                    if (!current->isObject()) return JSONValue(nullptr);
+                    auto& obj = current->asObject();
+                    auto it = obj.find(key);
+                    if (it == obj.end()) return JSONValue(nullptr);
+                    current = &it->second;
+                }
+
+                std::string idxStr = segment.substr(bracketPos + 1);
+                size_t closeBracket = idxStr.find(']');
+                if (closeBracket != std::string::npos) {
+                    idxStr = idxStr.substr(0, closeBracket);
+                }
+
+                if (!current->isArray()) return JSONValue(nullptr);
+                auto& arr = current->asArray();
+                size_t idx = std::stoul(idxStr);
+                if (idx >= arr.size()) return JSONValue(nullptr);
+                current = &arr[idx];
+            } else {
+                if (!current->isObject()) return JSONValue(nullptr);
+                auto& obj = current->asObject();
+                auto it = obj.find(segment);
+                if (it == obj.end()) return JSONValue(nullptr);
+                current = &it->second;
+            }
+        }
+
+        return *current;
+    }
+
+    std::vector<JSONValue> JSONValue::queryAll(const JSONValue& root, const std::string& path) {
+        std::vector<JSONValue> results;
+
+        if (path.find('*') == std::string::npos) {
+            results.push_back(query(root, path));
+            return results;
+        }
+
+        std::string p = path;
+        if (p[0] == '$') {
+            if (p.size() > 1) p = p.substr(1);
+        }
+        if (p[0] == '.') p = p.substr(1);
+
+        size_t wildcard = p.find('*');
+        if (wildcard == std::string::npos) {
+            results.push_back(query(root, path));
+            return results;
+        }
+
+        std::string prefix = p.substr(0, wildcard);
+        JSONValue prefixResult = query(root, prefix);
+
+        if (prefixResult.isArray()) {
+            for (auto& item : prefixResult.asArray()) {
+                results.push_back(item);
+            }
+        } else if (prefixResult.isObject()) {
+            for (auto& [key, val] : prefixResult.asObject()) {
+                results.push_back(val);
+            }
+        }
+
+        return results;
+    }
+
+    JSONValue JSONValue::merge(const JSONValue& base, const JSONValue& override_) {
+        if (!base.isObject() || !override_.isObject()) {
+            return override_;
+        }
+
+        JSONValue result = base;
+        auto& resultObj = std::get<JSONObject>(result.m_data);
+
+        for (auto& [key, val] : override_.asObject()) {
+            auto baseIt = resultObj.find(key);
+            if (baseIt != resultObj.end() && baseIt->second.isObject() && val.isObject()) {
+                resultObj[key] = merge(baseIt->second, val);
+            } else {
+                resultObj[key] = val;
+            }
+        }
+
+        return result;
+    }
+
+    JSONValue JSONValue::diff(const JSONValue& a, const JSONValue& b) {
+        JSONObject diffObj;
+
+        if (a.getType() != b.getType()) {
+            diffObj["_typeChanged"] = JSONValue(true);
+            diffObj["_oldType"] = JSONValue(static_cast<int>(a.getType()));
+            diffObj["_newType"] = JSONValue(static_cast<int>(b.getType()));
+            return JSONValue(diffObj);
+        }
+
+        if (a.isObject() && b.isObject()) {
+            auto& aObj = a.asObject();
+            auto& bObj = b.asObject();
+
+            for (auto& [key, val] : aObj) {
+                auto bIt = bObj.find(key);
+                if (bIt == bObj.end()) {
+                    diffObj[key] = JSONObject{{"_removed", val}};
+                } else {
+                    JSONValue d = diff(val, bIt->second);
+                    if (!d.isObject() || d.size() > 0) {
+                        diffObj[key] = d;
+                    }
+                }
+            }
+
+            for (auto& [key, val] : bObj) {
+                if (aObj.find(key) == aObj.end()) {
+                    diffObj[key] = JSONObject{{"_added", val}};
+                }
+            }
+        } else if (a.isArray() && b.isArray()) {
+            auto& aArr = a.asArray();
+            auto& bArr = b.asArray();
+
+            size_t maxSize = std::max(aArr.size(), bArr.size());
+            for (size_t i = 0; i < maxSize; ++i) {
+                std::string idxKey = std::to_string(i);
+                if (i >= aArr.size()) {
+                    diffObj[idxKey] = JSONObject{{"_added", bArr[i]}};
+                } else if (i >= bArr.size()) {
+                    diffObj[idxKey] = JSONObject{{"_removed", aArr[i]}};
+                } else {
+                    JSONValue d = diff(aArr[i], bArr[i]);
+                    if (!d.isObject() || d.size() > 0) {
+                        diffObj[idxKey] = d;
+                    }
+                }
+            }
+        } else {
+            if (a.toString() != b.toString()) {
+                diffObj["_old"] = a;
+                diffObj["_new"] = b;
+            }
+        }
+
+        return JSONValue(diffObj);
+    }
+
+    JSONValue JSONValue::patch(const JSONValue& base, const JSONValue& patchData) {
+        if (!patchData.isObject()) return patchData;
+        if (!base.isObject()) return patchData;
+
+        JSONValue result = base;
+        auto& resultObj = std::get<JSONObject>(result.m_data);
+
+        for (auto& [key, val] : patchData.asObject()) {
+            if (val.isObject() && val.hasKey("_removed")) {
+                resultObj.erase(key);
+            } else if (val.isObject() && val.hasKey("_added")) {
+                resultObj[key] = val.get("_added");
+            } else if (val.isObject() && resultObj.find(key) != resultObj.end() && resultObj[key].isObject()) {
+                resultObj[key] = patch(resultObj[key], val);
+            } else {
+                resultObj[key] = val;
+            }
+        }
+
+        return result;
+    }
+
+    bool JSONValue::validateSchema(const JSONValue& value, const JSONValue& schema, std::string* errorOut) {
+        if (!schema.isObject()) return true;
+
+        auto& schemaObj = schema.asObject();
+
+        if (schema.hasKey("type")) {
+            std::string expectedType = schema.get("type").asString();
+            std::string actualType;
+
+            switch (value.getType()) {
+                case JSONType::Null:   actualType = "null"; break;
+                case JSONType::Bool:   actualType = "boolean"; break;
+                case JSONType::Number: actualType = "number"; break;
+                case JSONType::String: actualType = "string"; break;
+                case JSONType::Array:  actualType = "array"; break;
+                case JSONType::Object: actualType = "object"; break;
+            }
+
+            if (expectedType == "integer" && actualType == "number") {
+                double num = value.asNumber();
+                if (num != static_cast<int64_t>(num)) {
+                    if (errorOut) *errorOut = "Expected integer, got float";
+                    return false;
+                }
+            } else if (expectedType != actualType) {
+                if (errorOut) *errorOut = "Expected type " + expectedType + ", got " + actualType;
+                return false;
+            }
+        }
+
+        if (schema.hasKey("properties") && value.isObject()) {
+            auto& props = schema.get("properties").asObject();
+            for (auto& [propName, propSchema] : props) {
+                if (value.hasKey(propName)) {
+                    if (!validateSchema(value.get(propName), propSchema, errorOut)) {
+                        if (errorOut) *errorOut = "Property '" + propName + "': " + *errorOut;
+                        return false;
+                    }
+                } else if (propSchema.isObject() && propSchema.hasKey("required")) {
+                    bool required = propSchema.get("required").asBool();
+                    if (required) {
+                        if (errorOut) *errorOut = "Missing required property '" + propName + "'";
+                        return false;
+                    }
+                }
+            }
+        }
+
+        if (schema.hasKey("items") && value.isArray()) {
+            auto& items = schema.get("items");
+            for (auto& item : value.asArray()) {
+                if (!validateSchema(item, items, errorOut)) {
+                    return false;
+                }
+            }
+        }
+
+        if (schema.hasKey("enum") && schema.get("enum").isArray()) {
+            bool found = false;
+            for (auto& enumVal : schema.get("enum").asArray()) {
+                if (enumVal.toString() == value.toString()) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                if (errorOut) *errorOut = "Value not in enum";
+                return false;
+            }
+        }
+
+        if (schema.hasKey("minimum") && value.isNumber()) {
+            double min = schema.get("minimum").asNumber();
+            if (value.asNumber() < min) {
+                if (errorOut) *errorOut = "Value below minimum " + std::to_string(min);
+                return false;
+            }
+        }
+
+        if (schema.hasKey("maximum") && value.isNumber()) {
+            double max = schema.get("maximum").asNumber();
+            if (value.asNumber() > max) {
+                if (errorOut) *errorOut = "Value above maximum " + std::to_string(max);
+                return false;
+            }
+        }
+
+        if (schema.hasKey("minLength") && value.isString()) {
+            size_t minLen = static_cast<size_t>(schema.get("minLength").asInt());
+            if (value.asString().size() < minLen) {
+                if (errorOut) *errorOut = "String too short";
+                return false;
+            }
+        }
+
+        if (schema.hasKey("maxLength") && value.isString()) {
+            size_t maxLen = static_cast<size_t>(schema.get("maxLength").asInt());
+            if (value.asString().size() > maxLen) {
+                if (errorOut) *errorOut = "String too long";
+                return false;
+            }
+        }
+
+        if (schema.hasKey("pattern") && value.isString()) {
+            std::string pattern = schema.get("pattern").asString();
+            try {
+                std::regex re(pattern);
+                if (!std::regex_match(value.asString(), re)) {
+                    if (errorOut) *errorOut = "String does not match pattern";
+                    return false;
+                }
+            } catch (...) {
+                if (errorOut) *errorOut = "Invalid regex pattern";
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    JSONValue::StreamingParser::StreamingParser()
+        : m_braceDepth(0), m_bracketDepth(0), m_inString(false) {}
+
+    void JSONValue::StreamingParser::feed(const std::string& chunk) {
+        m_buffer += chunk;
+
+        for (size_t i = 0; i < chunk.size(); ++i) {
+            char c = chunk[i];
+
+            if (m_inString) {
+                if (c == '\\' && i + 1 < chunk.size()) {
+                    ++i;
+                    continue;
+                }
+                if (c == '"') {
+                    m_inString = false;
+                }
+                continue;
+            }
+
+            switch (c) {
+                case '"':
+                    m_inString = true;
+                    break;
+                case '{':
+                    m_braceDepth++;
+                    break;
+                case '}':
+                    m_braceDepth--;
+                    if (m_braceDepth == 0 && m_bracketDepth == 0) {
+                        size_t endPos = m_buffer.size() - (chunk.size() - i);
+                        std::string complete = m_buffer.substr(0, endPos + 1);
+                        try {
+                            m_completedValues.push_back(JSONValue::parse(complete));
+                            m_buffer = m_buffer.substr(endPos + 1);
+                        } catch (...) {}
+                    }
+                    break;
+                case '[':
+                    m_bracketDepth++;
+                    break;
+                case ']':
+                    m_bracketDepth--;
+                    if (m_braceDepth == 0 && m_bracketDepth == 0) {
+                        size_t endPos = m_buffer.size() - (chunk.size() - i);
+                        std::string complete = m_buffer.substr(0, endPos + 1);
+                        try {
+                            m_completedValues.push_back(JSONValue::parse(complete));
+                            m_buffer = m_buffer.substr(endPos + 1);
+                        } catch (...) {}
+                    }
+                    break;
+            }
+        }
+    }
+
+    bool JSONValue::StreamingParser::hasValue() const {
+        return !m_completedValues.empty();
+    }
+
+    JSONValue JSONValue::StreamingParser::popValue() {
+        if (m_completedValues.empty()) return JSONValue(nullptr);
+        JSONValue val = m_completedValues.front();
+        m_completedValues.erase(m_completedValues.begin());
+        return val;
+    }
+
+    void JSONValue::StreamingParser::reset() {
+        m_buffer.clear();
+        m_completedValues.clear();
+        m_braceDepth = 0;
+        m_bracketDepth = 0;
+        m_inString = false;
     }
 
 }
