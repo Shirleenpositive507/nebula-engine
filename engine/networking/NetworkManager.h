caea465fce012008edbe1fbecac0f8ba0ba8baf0
heheboobes <heheboobes@example.com>
2023-07-01 12:00:00 +0000

    feat: add connection pooling and network stats

--- engine/networking/NetworkManager.h
+++ engine/networking/NetworkManager.h
@@ -15,6 +15,7 @@
 #include <queue>
 #include <thread>
 #include <atomic>
+#include <deque>
 
 namespace nebula {
 
@@ -25,6 +26,17 @@ namespace nebula {
         uint64_t lastHeartbeat;
         uint64_t connectTime;
         bool connected;
+        uint32_t reconnectionAttempts;
+        uint64_t lastReconnectAttempt;
+    };
+
+    struct PooledConnection {
+        TCPSocket socket;
+        std::string host;
+        uint16_t port;
+        bool inUse;
+        uint64_t lastUsed;
+        uint64_t createdAt;
     };
 
     struct NetworkStats {
@@ -38,6 +50,12 @@ namespace nebula {
         uint64_t bytesReceivedPerSecond = 0;
         uint32_t pingMs = 0;
         uint32_t packetLoss = 0;
+        double latencyMs = 0.0;
+        double jitterMs = 0.0;
+        uint64_t totalBytesEncrypted = 0;
+        uint64_t totalBytesDecrypted = 0;
+        uint32_t activeConnections = 0;
+        uint32_t pooledConnections = 0;
     };
 
     class NetworkManager {
@@ -50,6 +68,7 @@ namespace nebula {
 
         bool connect(const std::string& host, uint16_t port);
         void disconnect();
+        void disconnectAll();
 
         bool send(NetworkMessage& message);
         bool broadcast(NetworkMessage& message);
@@ -64,6 +83,27 @@ namespace nebula {
 
         void update();
 
+        void setEncryptionKey(const std::vector<uint8_t>& key);
+        void enableEncryption(bool enable);
+        bool isEncryptionEnabled() const;
+
+        void setBandwidthLimit(uint64_t bytesPerSecond);
+        uint64_t getBandwidthLimit() const;
+
+        void setAutoReconnect(bool enable);
+        bool isAutoReconnectEnabled() const;
+
+        void setMaxReconnectionAttempts(uint32_t attempts);
+        uint32_t getMaxReconnectionAttempts() const;
+
+        void setConnectionPoolSize(uint32_t size);
+        uint32_t getConnectionPoolSize() const;
+
+        uint32_t getActiveConnectionCount() const;
+
+        void enableLatencyJitterMeasurement(bool enable);
+        bool isLatencyJitterMeasurementEnabled() const;
+
     private:
         NetworkManager();
         ~NetworkManager();
@@ -77,6 +117,13 @@ namespace nebula {
         void sendHeartbeat();
         void checkTimeouts();
         void resendUnacknowledged();
+        void attemptReconnection();
+        void manageConnectionPool();
+        void encryptData(std::vector<uint8_t>& data);
+        void decryptData(std::vector<uint8_t>& data);
+
+        void measureLatency();
+        void updateJitter(double currentLatency);
 
         TCPSocket m_tcpSocket;
         TCPListener m_listener;
@@ -120,6 +167,35 @@ namespace nebula {
         uint64_t m_statsIntervalStart;
         uint64_t m_bytesSentThisInterval;
         uint64_t m_bytesReceivedThisInterval;
+
+        bool m_autoReconnect;
+        uint32_t m_maxReconnectionAttempts;
+        uint64_t m_reconnectDelayMs;
+        uint64_t m_lastReconnectTime;
+        uint32_t m_reconnectionAttempts;
+        std::string m_lastHost;
+        uint16_t m_lastPort;
+        static constexpr uint32_t DEFAULT_MAX_RECONNECT_ATTEMPTS = 10;
+        static constexpr uint64_t DEFAULT_RECONNECT_DELAY_MS = 2000;
+
+        std::vector<PooledConnection> m_connectionPool;
+        uint32_t m_maxPoolSize;
+        mutable std::mutex m_poolMutex;
+        static constexpr uint32_t DEFAULT_POOL_SIZE = 8;
+        static constexpr uint64_t POOL_CONNECTION_TTL_MS = 60000;
+
+        std::vector<uint8_t> m_encryptionKey;
+        bool m_encryptionEnabled;
+
+        uint64_t m_bandwidthLimitBytesPerSec;
+        uint64_t m_bandwidthUsedThisSecond;
+        uint64_t m_bandwidthIntervalStart;
+
+        bool m_latencyJitterEnabled;
+        double m_lastLatencyMs;
+        double m_lastJitterMs;
+        std::deque<double> m_latencyHistory;
+        static constexpr size_t LATENCY_HISTORY_SIZE = 50;
 
         static uint64_t getCurrentTimeMs();
     };
