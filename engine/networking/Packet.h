f5bc44bc5c324d4ad801452615d2153a6ed2228b
heheboobes <heheboobes@example.com>
2023-07-07 12:00:00 +0000

    feat: add packet fragmentation and compression

--- engine/networking/Packet.h
+++ engine/networking/Packet.h
@@ -5,8 +5,19 @@
 #include <cstdint>
 #include <vector>
 #include <type_traits>
+#include <unordered_map>
+#include <functional>
+#include <memory>
 
 namespace nebula {
+
+    struct PacketFragment {
+        uint32_t packetId;
+        uint16_t fragmentIndex;
+        uint16_t totalFragments;
+        uint16_t fragmentSize;
+        std::vector<uint8_t> data;
+    };
 
     class Packet {
     public:
@@ -17,6 +28,9 @@ namespace nebula {
 
         const void* getData() const;
         size_t getDataSize() const;
+
+        bool isFragmented() const { return m_fragmented; }
+        uint32_t getPacketId() const { return m_packetId; }
 
         template<typename T>
         void write(T value) {
@@ -58,11 +72,39 @@ namespace nebula {
         size_t getRemainingBytes() const;
         void reserve(size_t size);
 
+        void setSequenceNumber(uint16_t seq);
+        uint16_t getSequenceNumber() const;
+
+        void setChecksum(uint32_t crc);
+        uint32_t getChecksum() const;
+        bool verifyChecksum() const;
+        uint32_t calculateCRC32() const;
+
+        void compress();
+        void decompress();
+
+        void encrypt(const uint8_t* key, size_t keyLength);
+        void decrypt(const uint8_t* key, size_t keyLength);
+
+        std::vector<Packet> fragment(size_t maxFragmentSize);
+        bool reassemble(const std::vector<Packet>& fragments);
+        bool addFragment(const PacketFragment& fragment);
+        bool isReassemblyComplete() const;
+
         sf::Packet& getHandle() { return m_packet; }
         const sf::Packet& getHandle() const { return m_packet; }
 
     private:
+        void writeHeader();
+        void readHeader();
+
         sf::Packet m_packet;
+        uint16_t m_sequenceNumber;
+        uint32_t m_checksum;
+        bool m_fragmented;
+        uint32_t m_packetId;
+        std::vector<PacketFragment> m_fragments;
+        static uint32_t s_nextPacketId;
     };
 
     class PacketPool {
@@ -81,6 +123,27 @@ namespace nebula {
         std::vector<Packet> m_pool;
         std::vector<Packet*> m_freeList;
         size_t m_activeCount = 0;
+    };
+
+    class PacketFragmenter {
+    public:
+        PacketFragmenter(size_t maxFragmentSize = 1024);
+
+        std::vector<PacketFragment> fragment(const Packet& packet);
+        bool addFragment(const PacketFragment& fragment);
+        Packet reassemble(uint32_t packetId);
+        bool isComplete(uint32_t packetId) const;
+        void clear(uint32_t packetId);
+
+    private:
+        size_t m_maxFragmentSize;
+        struct ReassemblyBuffer {
+            std::vector<PacketFragment> fragments;
+            uint16_t totalFragments;
+            uint64_t lastReceiveTime;
+        };
+        std::unordered_map<uint32_t, ReassemblyBuffer> m_reassemblyBuffers;
+        static constexpr uint64_t FRAGMENT_TIMEOUT_MS = 5000;
     };
 
 }
