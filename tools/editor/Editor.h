a4fb9a3d3e823f1ec3188776a4a8405af39da220
heheboobes <heheboobes@example.com>
2023-07-09 12:00:00 +0000

    feat: add editor project system and layout save

--- tools/editor/Editor.h
+++ tools/editor/Editor.h
@@ -5,6 +5,8 @@
 #include <stack>
 #include <memory>
 #include <functional>
+#include <unordered_map>
+#include <fstream>
 
 #include "EditorViewport.h"
 #include "EditorGUI.h"
@@ -24,6 +26,63 @@ enum class EditorMode {
 struct UndoCommand {
     std::string type;
     std::string data;
+};
+
+struct EditorProject {
+    std::string name;
+    std::string version;
+    std::string author;
+    std::string lastScenePath;
+    std::vector<std::string> recentScenes;
+    std::vector<std::string> openScenes;
+    int activeSceneIndex;
+    uint64_t createdAt;
+    uint64_t lastModifiedAt;
+};
+
+struct EditorLayout {
+    std::unordered_map<std::string, sf::FloatRect> windowRects;
+    std::unordered_map<std::string, bool> windowVisibility;
+    std::unordered_map<std::string, std::string> dockTargets;
+    std::vector<std::string> tabOrder;
+    float hierarchyWidth;
+    float propertiesWidth;
+    float assetBrowserHeight;
+    float consoleHeight;
+};
+
+struct EditorSettings {
+    bool vsyncEnabled;
+    bool showGrid;
+    bool showRulers;
+    bool snapToGrid;
+    float snapSize;
+    unsigned int fontSize;
+    std::string theme;
+    sf::Color backgroundColor;
+    sf::Color gridColor;
+    float cameraPanSpeed;
+    float cameraZoomSpeed;
+    std::string defaultScenePath;
+    std::string defaultProjectPath;
+    bool autoSave;
+    float autoSaveInterval;
+};
+
+struct EditorShortcut {
+    sf::Keyboard::Key key;
+    bool ctrl;
+    bool shift;
+    bool alt;
+    std::string action;
+};
+
+struct SceneDocument {
+    int id;
+    std::string name;
+    std::string filePath;
+    bool modified;
+    bool active;
 };
 
 class Editor {
@@ -62,9 +121,43 @@ public:
     debug::DebugOverlay& getDebugOverlay();
     debug::Console& getConsole();
 
+    int createProject(const std::string& name, const std::string& path);
+    bool loadProject(const std::string& filename);
+    bool saveProject();
+    bool saveProjectAs(const std::string& filename);
+    EditorProject& getProject();
+    const EditorProject& getProject() const;
+
+    void loadSettings(const std::string& filename);
+    void saveSettings(const std::string& filename);
+    EditorSettings& getSettings();
+    const EditorSettings& getSettings() const;
+
+    void loadLayout(const std::string& filename);
+    void saveLayout(const std::string& filename);
+    EditorLayout& getLayout();
+    const EditorLayout& getLayout() const;
+
+    void setCustomShortcut(const std::string& action, sf::Keyboard::Key key,
+                           bool ctrl, bool shift, bool alt);
+    void removeCustomShortcut(const std::string& action);
+    const std::vector<EditorShortcut>& getCustomShortcuts() const;
+
+    int openSceneDocument(const std::string& name);
+    void closeSceneDocument(int docId);
+    void setActiveSceneDocument(int docId);
+    SceneDocument* getActiveSceneDocument();
+    std::vector<SceneDocument>& getOpenDocuments();
+    int getDocumentCount() const;
+
+    void setCustomShortcutFile(const std::string& filename);
+
 private:
     void handleShortcuts(const sf::Event& event);
     void updatePlayMode(float dt);
+    void autoSaveTimer();
+    void serializeProject(std::ofstream& file);
+    void deserializeProject(std::ifstream& file);
 
     EditorMode m_mode;
     bool m_initialized;
@@ -87,6 +180,17 @@ private:
     float m_cameraZoom;
 
     sf::Clock m_stepClock;
+
+    EditorProject m_project;
+    EditorLayout m_layout;
+    EditorSettings m_settings;
+    std::vector<EditorShortcut> m_customShortcuts;
+    std::vector<SceneDocument> m_openDocuments;
+    int m_nextDocId;
+    std::string m_projectFilePath;
+    std::string m_settingsFilePath;
+    std::string m_layoutFilePath;
+    std::string m_shortcutFilePath;
 };
 
 } // namespace editor
