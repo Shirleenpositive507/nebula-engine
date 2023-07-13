bde3eec86d48c456be2e1db028746885794be6ce
heheboobes <heheboobes@example.com>
2023-07-13 12:00:00 +0000

    feat: add transform gizmo and render modes

--- tools/editor/EditorViewport.h
+++ tools/editor/EditorViewport.h
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <functional>
+#include <unordered_map>
 
 namespace nebula {
 namespace tools {
@@ -12,6 +13,35 @@ struct SelectableObject {
     int id;
     sf::FloatRect bounds;
     bool selected;
+};
+
+enum class ViewportRenderMode {
+    Shaded,
+    Wireframe,
+    LightingOnly,
+    CollisionDebug,
+    ShadedWithGrid,
+    MaterialPreview
+};
+
+struct GizmoState {
+    bool active;
+    int type;
+    int axis;
+    sf::Vector2f dragStart;
+    sf::Vector2f currentPos;
+    bool dragging;
+    float snapStep;
+};
+
+struct ViewportCamera {
+    std::string name;
+    sf::Vector3f position;
+    sf::Vector3f target;
+    float fov;
+    float nearPlane;
+    float farPlane;
+    bool active;
 };
 
 class EditorViewport {
@@ -48,16 +78,39 @@ public:
 
     sf::Sprite getRenderSprite() const;
 
+    void setRenderMode(ViewportRenderMode mode);
+    ViewportRenderMode getRenderMode() const;
+    void cycleRenderMode();
+
+    void enableGizmo(bool enable);
+    bool isGizmoEnabled() const;
+    void setGizmoType(int type);
+    int getGizmoType() const;
+    GizmoState& getGizmoState();
+    void snapToGrid(sf::Vector2f& pos) const;
+
+    int addCamera(const std::string& name);
+    void removeCamera(int cameraId);
+    void setActiveCamera(int cameraId);
+    ViewportCamera* getActiveCamera();
+    size_t getCameraCount() const;
+
+    sf::Vector2f getGizmoPosition() const;
+    void setGizmoPosition(const sf::Vector2f& pos);
+
 private:
     void renderGrid(sf::RenderWindow& window);
     void renderOriginAxes(sf::RenderWindow& window);
     void renderSelection(sf::RenderWindow& window, const std::vector<SelectableObject>& objects);
     void renderRulers(sf::RenderWindow& window);
     void renderPixelCoordinates(sf::RenderWindow& window);
+    void renderGizmo(sf::RenderWindow& window);
+    void renderWireframeOverlay(sf::RenderWindow& window);
 
     void handleCameraPan(const sf::Event& event);
     void handleCameraZoom(const sf::Event& event);
     void handleObjectSelection(const sf::Event& event, const std::vector<SelectableObject>& objects);
+    void handleGizmoDrag(const sf::Event& event);
 
     sf::RenderTexture m_renderTexture;
     sf::View m_view;
@@ -78,6 +131,14 @@ private:
     std::vector<int> m_selectedIds;
 
     sf::Font m_font;
+
+    ViewportRenderMode m_renderMode;
+    GizmoState m_gizmo;
+    bool m_gizmoEnabled;
+
+    std::vector<ViewportCamera> m_cameras;
+    int m_activeCameraIndex;
+    int m_nextCameraId;
 };
 
 } // namespace editor
