fe369f67939a6507b21d8167bf2a662bbc7aa05e
heheboobes <heheboobes@dev.null>
2023-12-13 12:00:00 +1000

    feat: add sprite sheet import and animation events

--- engine/graphics/Animation.h
+++ engine/graphics/Animation.h
@@ -3,6 +3,9 @@
 #include <SFML/Graphics/Rect.hpp>
 #include <vector>
 #include <string>
+#include <functional>
+#include <unordered_map>
+#include <memory>
 
 namespace nebula {
     namespace graphics {
@@ -10,9 +13,32 @@ namespace nebula {
         struct Frame {
             sf::IntRect rect;
             float duration;
+            float speedMultiplier;
 
-            Frame() : rect(0, 0, 0, 0), duration(0.1f) {}
-            Frame(const sf::IntRect& r, float d) : rect(r), duration(d) {}
+            Frame() : rect(0, 0, 0, 0), duration(0.1f), speedMultiplier(1.0f) {}
+            Frame(const sf::IntRect& r, float d, float speed = 1.0f)
+                : rect(r), duration(d), speedMultiplier(speed) {}
+        };
+
+        struct AnimationEvent {
+            std::string name;
+            float triggerTime;
+            std::function<void()> callback;
+            bool triggered;
+
+            AnimationEvent() : triggerTime(0.f), triggered(false) {}
+            AnimationEvent(const std::string& n, float t, std::function<void()> cb)
+                : name(n), triggerTime(t), callback(cb), triggered(false) {}
+        };
+
+        class SpriteSheetParser {
+        public:
+            static bool parseAsepriteJson(const std::string& jsonPath,
+                                          std::vector<Frame>& outFrames,
+                                          std::unordered_map<std::string, std::vector<size_t>>& outTags);
+            static bool parseSpriterJson(const std::string& jsonPath,
+                                         std::vector<Frame>& outFrames,
+                                         std::unordered_map<std::string, std::vector<size_t>>& outAnimations);
         };
 
         class Animation {
@@ -24,6 +50,14 @@ namespace nebula {
 
             void addFrame(const sf::IntRect& rect, float duration);
             void addFrame(const Frame& frame);
+
+            void addEvent(const AnimationEvent& event);
+            void addEvent(const std::string& name, float triggerTime, std::function<void()> callback);
+            void clearEvents();
+            const std::vector<AnimationEvent>& getEvents() const;
+
+            void setFrameSpeedMultiplier(std::size_t index, float multiplier);
+            float getFrameSpeedMultiplier(std::size_t index) const;
 
             void play();
             void pause();
@@ -52,11 +86,14 @@ namespace nebula {
         private:
             std::string m_name;
             std::vector<Frame> m_frames;
+            std::vector<AnimationEvent> m_events;
             std::size_t m_currentFrame;
             float m_elapsed;
             bool m_playing;
             bool m_loop;
             bool m_finished;
+
+            void checkEvents(float prevElapsed, float currentElapsed);
         };
 
     }
