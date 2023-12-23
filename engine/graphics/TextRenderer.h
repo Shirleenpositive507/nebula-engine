2820aa0839b1b9356a322982dfac82a87b3bcb73
heheboobes <heheboobes@dev.null>
2023-12-23 12:00:00 +1000

    feat: add rich text markup and text effects

--- engine/graphics/TextRenderer.h
+++ engine/graphics/TextRenderer.h
@@ -7,6 +7,8 @@
 #include <SFML/System/Vector2.hpp>
 #include <string>
 #include <vector>
+#include <memory>
+#include <stack>
 
 namespace nebula {
     namespace graphics {
@@ -33,6 +35,26 @@ namespace nebula {
             return static_cast<TextStyle>(static_cast<int>(a) & static_cast<int>(b));
         }
 
+        struct RichTextSpan {
+            std::string text;
+            sf::Color color;
+            TextStyle style;
+            unsigned int fontSize;
+            bool isBold;
+            bool isItalic;
+
+            RichTextSpan() : color(255, 255, 255), style(TextStyle::Regular), fontSize(14),
+                isBold(false), isItalic(false) {}
+        };
+
+        struct MarkupTag {
+            std::string name;
+            std::unordered_map<std::string, std::string> attributes;
+            bool closing;
+
+            MarkupTag() : closing(false) {}
+        };
+
         class TextRenderer {
         public:
             TextRenderer();
@@ -45,6 +67,10 @@ namespace nebula {
 
             void setString(const std::string& text);
             const std::string& getString() const;
+
+            void setRichText(const std::string& markupText);
+            bool isRichText() const;
+            void setRichTextEnabled(bool enabled);
 
             void setCharacterSize(unsigned int size);
             unsigned int getCharacterSize() const;
@@ -62,11 +88,20 @@ namespace nebula {
             void setOutlineThickness(float thickness);
             float getOutlineThickness() const;
 
+            void setDropShadow(const sf::Vector2f& offset, const sf::Color& color, float blurRadius = 0.f);
+            void disableDropShadow();
+            bool hasDropShadow() const;
+
             void setLetterSpacing(float spacingFactor);
             float getLetterSpacing() const;
 
             void setLineSpacing(float spacingFactor);
             float getLineSpacing() const;
+
+            void setEmojiFont(const std::shared_ptr<Font>& emojiFont);
+            std::shared_ptr<Font> getEmojiFont() const;
+            void setEmojiSupportEnabled(bool enabled);
+            bool isEmojiSupportEnabled() const;
 
             sf::FloatRect getLocalBounds() const;
             sf::FloatRect getGlobalBounds() const;
@@ -102,9 +137,26 @@ namespace nebula {
             bool m_dirty;
             std::vector<std::string> m_lines;
 
+            bool m_richTextEnabled;
+            std::string m_markupSource;
+            std::vector<RichTextSpan> m_richSpans;
+
+            bool m_dropShadowEnabled;
+            sf::Vector2f m_shadowOffset;
+            sf::Color m_shadowColor;
+            float m_shadowBlur;
+
+            bool m_emojiEnabled;
+            std::shared_ptr<Font> m_emojiFont;
+
             void rebuild();
             std::vector<std::string> wrapText(const std::string& text, float maxWidth) const;
             void applyAlignment();
+            void parseMarkup(const std::string& markup);
+            std::vector<RichTextSpan> tokenizeMarkup(const std::string& markup) const;
+            void buildRichTextGeometry();
+            bool hasEmoji(const std::string& text) const;
+            std::string replaceEmojiWithPlaceholder(const std::string& text) const;
         };
 
     }
